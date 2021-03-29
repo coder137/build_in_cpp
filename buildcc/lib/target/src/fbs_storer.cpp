91025883746e926b8b3b33735de04bad564c03dc
Niket Naidu <coder137@gmail.com>
2021-03-29 02:39:13 -0700

    Updated fbs_storer to store lib_deps

--- buildcc/lib/target/src/fbs_storer.cpp
+++ buildcc/lib/target/src/fbs_storer.cpp
@@ -28,19 +28,17 @@ fbs::TargetType get_fbs_target_type(buildcc::base::TargetType type) {
   return target_type;
 }
 
-// TODO, Improve this
+// TODO, Complete this with additional flags
 flatbuffers::Offset<fbs::Toolchain>
 get_fbs_toolchain(flatbuffers::FlatBufferBuilder &builder,
                   const buildcc::Toolchain &toolchain) {
-  auto fbs_toolchain_name = builder.CreateString(toolchain.GetName());
-  auto fbs_c_compiler = builder.CreateString(toolchain.GetCCompiler());
-  auto fbs_cpp_compiler = builder.CreateString(toolchain.GetCppCompiler());
   // auto fbs_preprocessor_flags;
   // auto fbs_c_compile_flags;
   // auto fbs_cpp_compile_flags;
   // auto fbs_link_flags;
-  return fbs::CreateToolchain(builder, fbs_toolchain_name, fbs_c_compiler,
-                              fbs_cpp_compiler);
+  return fbs::CreateToolchainDirect(builder, toolchain.GetName().c_str(),
+                                    toolchain.GetCCompiler().c_str(),
+                                    toolchain.GetCppCompiler().c_str());
 }
 
 std::vector<flatbuffers::Offset<fbs::Path>>
@@ -69,10 +67,13 @@ bool Target::Store() {
   auto fbs_toolchain = get_fbs_toolchain(builder, toolchain_);
   auto fbs_source_files = get_fbs_path(builder, current_source_files_);
   auto fbs_include_dirs = get_fbs_path(builder, current_include_dirs_);
+  auto fbs_lib_deps = get_fbs_path(builder, current_lib_deps_);
+  // lib_dirs
 
   auto fbs_target = fbs::CreateTargetDirect(
       builder, name_.c_str(), target_intermediate_dir_.string().c_str(),
-      fbs_target_type, fbs_toolchain, &fbs_source_files, &fbs_include_dirs);
+      fbs_target_type, fbs_toolchain, &fbs_source_files, &fbs_include_dirs,
+      &fbs_lib_deps);
   fbs::FinishTargetBuffer(builder, fbs_target);
 
   auto file_path = GetBinaryPath();
