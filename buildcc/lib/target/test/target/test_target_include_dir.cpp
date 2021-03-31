f9ad4b6da5f2e039604f06b106ebca500e4110ad
Niket Naidu <coder137@gmail.com>
2021-03-31 03:08:15 -0700

    test_target modular with test_target_include_dir

---
+++ buildcc/lib/target/test/target/test_target_include_dir.cpp
@@ -0,0 +1,159 @@
+#include "constants.h"
+
+#include "base/target.h"
+
+#include "env.h"
+
+// Third Party
+#include "flatbuffers/util.h"
+
+// NOTE, Make sure all these includes are AFTER the system and header includes
+#include "CppUTest/CommandLineTestRunner.h"
+#include "CppUTest/MemoryLeakDetectorNewMacros.h"
+#include "CppUTest/TestHarness.h"
+#include "CppUTest/Utest.h"
+
+// clang-format off
+TEST_GROUP(TargetTestIncludeDirGroup)
+{
+};
+// clang-format on
+
+static fs::path target_include_dir_intermediate_path =
+    fs::path(BUILD_TARGET_INCLUDE_DIR_INTERMEDIATE_DIR);
+
+TEST(TargetTestIncludeDirGroup, TargetBuildIncludeDir) {
+  constexpr const char *const NAME = "IncludeDir.exe";
+  constexpr const char *const BIN = "IncludeDir.exe.bin";
+
+  constexpr const char *const DUMMY_MAIN_C = "dummy_main.c";
+  constexpr const char *const RELATIVE_INCLUDE_DIR = "include";
+  constexpr const char *const INCLUDE_HEADER_SOURCE = "include_header.cpp";
+  constexpr const char *const INCLUDE_HEADER_INCLUDE = "include_header.h";
+
+  auto source_path = fs::path(BUILD_SCRIPT_SOURCE) / "data";
+  auto intermediate_path = target_include_dir_intermediate_path / NAME;
+
+  // Delete
+  fs::remove_all(intermediate_path);
+
+  auto dummy_c_file =
+      buildcc::internal::Path::CreateExistingPath((source_path / DUMMY_MAIN_C));
+  auto include_header_file = buildcc::internal::Path::CreateExistingPath(
+      (source_path / INCLUDE_HEADER_SOURCE));
+  auto include_header_path = buildcc::internal::Path::CreateExistingPath(
+      (source_path / RELATIVE_INCLUDE_DIR));
+
+  {
+    buildcc::base::Target include_compile(
+        NAME, buildcc::base::TargetType::Executable,
+        buildcc::Toolchain("gcc", "gcc", "g++"), "data");
+    include_compile.AddSource(DUMMY_MAIN_C);
+    include_compile.AddSource(INCLUDE_HEADER_SOURCE);
+    include_compile.AddIncludeDir(RELATIVE_INCLUDE_DIR);
+    // Duplicate include directory
+    include_compile.AddIncludeDir(RELATIVE_INCLUDE_DIR);
+    include_compile.Build();
+
+    buildcc::internal::FbsLoader loader(NAME, intermediate_path);
+    bool is_loaded = loader.Load();
+    CHECK_TRUE(is_loaded);
+    const auto &loaded_sources = loader.GetLoadedSources();
+    const auto &loaded_dirs = loader.GetLoadedIncludeDirs();
+
+    CHECK_EQUAL(loaded_sources.size(), 2);
+    CHECK_EQUAL(loaded_dirs.size(), 1);
+
+    CHECK_FALSE(loaded_sources.find(dummy_c_file) == loaded_sources.end());
+    CHECK_FALSE(loaded_sources.find(include_header_file) ==
+                loaded_sources.end());
+    CHECK_FALSE(loaded_dirs.find(include_header_path) == loaded_dirs.end());
+  }
+  {
+    // * 1 Adding new include directory
+    buildcc::base::Target include_compile(
+        NAME, buildcc::base::TargetType::Executable,
+        buildcc::Toolchain("gcc", "gcc", "g++"), "data");
+    include_compile.AddSource(DUMMY_MAIN_C);
+    include_compile.AddSource(INCLUDE_HEADER_SOURCE);
+    include_compile.AddIncludeDir(RELATIVE_INCLUDE_DIR);
+    // Adds the data directory
+    include_compile.AddIncludeDir("");
+    include_compile.Build();
+
+    buildcc::internal::FbsLoader loader(NAME, intermediate_path);
+    bool is_loaded = loader.Load();
+    CHECK_TRUE(is_loaded);
+    const auto &loaded_sources = loader.GetLoadedSources();
+    const auto &loaded_dirs = loader.GetLoadedIncludeDirs();
+
+    CHECK_EQUAL(loaded_sources.size(), 2);
+    CHECK_EQUAL(loaded_dirs.size(), 2);
+
+    CHECK_FALSE(loaded_sources.find(dummy_c_file) == loaded_sources.end());
+    CHECK_FALSE(loaded_sources.find(include_header_file) ==
+                loaded_sources.end());
+    CHECK_FALSE(loaded_dirs.find(include_header_path) == loaded_dirs.end());
+  }
+  {
+    // * Force copy to trigger recompile when HEADER changes
+    // *2 Current file is updated
+    auto file_path =
+        source_path / RELATIVE_INCLUDE_DIR / INCLUDE_HEADER_INCLUDE;
+    flatbuffers::SaveFile(file_path.string().c_str(), std::string{""}, false);
+
+    buildcc::base::Target include_compile(
+        NAME, buildcc::base::TargetType::Executable,
+        buildcc::Toolchain("gcc", "gcc", "g++"), "data");
+    include_compile.AddSource(DUMMY_MAIN_C);
+    include_compile.AddSource(INCLUDE_HEADER_SOURCE);
+    include_compile.AddIncludeDir(RELATIVE_INCLUDE_DIR);
+    include_compile.AddIncludeDir("");
+    include_compile.Build();
+
+    buildcc::internal::FbsLoader loader(NAME, intermediate_path);
+    bool is_loaded = loader.Load();
+    CHECK_TRUE(is_loaded);
+    const auto &loaded_sources = loader.GetLoadedSources();
+    const auto &loaded_dirs = loader.GetLoadedIncludeDirs();
+
+    CHECK_EQUAL(loaded_sources.size(), 2);
+    CHECK_EQUAL(loaded_dirs.size(), 2);
+
+    CHECK_FALSE(loaded_sources.find(dummy_c_file) == loaded_sources.end());
+    CHECK_FALSE(loaded_sources.find(include_header_file) ==
+                loaded_sources.end());
+    CHECK_FALSE(loaded_dirs.find(include_header_path) == loaded_dirs.end());
+  }
+  {
+    // * Remove include directory
+    buildcc::base::Target include_compile(
+        NAME, buildcc::base::TargetType::Executable,
+        buildcc::Toolchain("gcc", "gcc", "g++"), "data");
+    include_compile.AddSource(DUMMY_MAIN_C);
+    include_compile.AddSource(INCLUDE_HEADER_SOURCE);
+    include_compile.AddIncludeDir(RELATIVE_INCLUDE_DIR);
+    include_compile.Build();
+
+    buildcc::internal::FbsLoader loader(NAME, intermediate_path);
+    bool is_loaded = loader.Load();
+    CHECK_TRUE(is_loaded);
+    const auto &loaded_sources = loader.GetLoadedSources();
+    const auto &loaded_dirs = loader.GetLoadedIncludeDirs();
+
+    CHECK_EQUAL(loaded_sources.size(), 2);
+    CHECK_EQUAL(loaded_dirs.size(), 1);
+
+    CHECK_FALSE(loaded_sources.find(dummy_c_file) == loaded_sources.end());
+    CHECK_FALSE(loaded_sources.find(include_header_file) ==
+                loaded_sources.end());
+    CHECK_FALSE(loaded_dirs.find(include_header_path) == loaded_dirs.end());
+  }
+}
+
+int main(int ac, char **av) {
+  MemoryLeakWarningPlugin::turnOffNewDeleteOverloads();
+  buildcc::env::init(BUILD_SCRIPT_SOURCE,
+                     BUILD_TARGET_INCLUDE_DIR_INTERMEDIATE_DIR);
+  return CommandLineTestRunner::RunAllTests(ac, av);
+}
