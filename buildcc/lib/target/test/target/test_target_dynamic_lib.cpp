d468e592592d9c3fc07f3c69c9faf5a7f58bbccf
Niket Naidu <coder137@gmail.com>
2021-03-31 17:53:23 -0700

    test_target modular with test_target_dynamic_lib

---
+++ buildcc/lib/target/test/target/test_target_dynamic_lib.cpp
@@ -0,0 +1,44 @@
+#include "constants.h"
+
+#include "base/target.h"
+
+#include "env.h"
+
+//
+#include "dynamic_target.h"
+
+// NOTE, Make sure all these includes are AFTER the system and header includes
+#include "CppUTest/CommandLineTestRunner.h"
+#include "CppUTest/MemoryLeakDetectorNewMacros.h"
+#include "CppUTest/TestHarness.h"
+#include "CppUTest/Utest.h"
+
+// clang-format off
+TEST_GROUP(TargetTestDynamicLibGroup)
+{
+};
+// clang-format on
+
+static fs::path intermediate_path =
+    fs::path(BUILD_TARGET_DYNAMIC_LIB_INTERMEDIATE_DIR);
+
+TEST(TargetTestDynamicLibGroup, DynamicLibrarySimple) {
+  constexpr const char *const DYNAMIC_TARGET = "libDynamicTest.so";
+
+  fs::remove_all(intermediate_path / DYNAMIC_TARGET);
+
+  {
+    buildcc::Toolchain gcc("gcc", "gcc", "g++");
+    buildcc::DynamicTarget dynamic_target(DYNAMIC_TARGET, gcc, "");
+    dynamic_target.AddSource("data/include_header.cpp");
+    dynamic_target.AddIncludeDir("data/include");
+    dynamic_target.Build();
+  }
+}
+
+int main(int ac, char **av) {
+  MemoryLeakWarningPlugin::turnOffNewDeleteOverloads();
+  buildcc::env::init(BUILD_SCRIPT_SOURCE,
+                     BUILD_TARGET_DYNAMIC_LIB_INTERMEDIATE_DIR);
+  return CommandLineTestRunner::RunAllTests(ac, av);
+}
