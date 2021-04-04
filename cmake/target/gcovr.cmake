0d768778ca52754336a748716e053fd082365b82
Niket Naidu <coder137@gmail.com>
2021-04-04 02:19:07 -0700

    Update gcovr.cmake
    Updated gcovr.cmake with proper cmake paths

--- cmake/target/gcovr.cmake
+++ cmake/target/gcovr.cmake
@@ -14,21 +14,21 @@ set(GCOVR_REMOVE_OPTIONS
 )
 
 # TODO, Update
-file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/gcovr_coverage)
+file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/gcovr_coverage)
 set(GCOVR_VERBOSE -v -s)
 set(GCOVR_UNNECESSARY_ARCS 
     --exclude-unreachable-branches
     --exclude-throw-branches
 )
 set(GCOVR_SEARCH_PATHS
-    ${CMAKE_CURRENT_BINARY_DIR}
+    ${CMAKE_BINARY_DIR}
 )
-set(GCOVR_GCOV_EXE --gcov-executable "gcov")
+set(GCOVR_DIR ${CMAKE_BINARY_DIR}/gcovr_coverage)
 add_custom_target(gcovr_coverage
-    COMMAND cmake --build ${CMAKE_CURRENT_BINARY_DIR}
-    COMMAND cmake --build ${CMAKE_CURRENT_BINARY_DIR} --target test
 
-    COMMAND ${gcovr_program} -r ${CMAKE_CURRENT_SOURCE_DIR}/buildcc --html-details ${CMAKE_CURRENT_BINARY_DIR}/gcovr_coverage/gcovr.html ${GCOVR_GCOV_EXE} ${GCOVR_VERBOSE} ${GCOVR_UNNECESSARY_ARCS} ${GCOVR_REMOVE_OPTIONS} ${GCOVR_SEARCH_PATHS}
+    COMMAND ${gcovr_program} -r ${CMAKE_CURRENT_SOURCE_DIR} --html-details ${GCOVR_DIR}/gcovr.html --object-directory ${GCOVR_DIR} ${GCOVR_VERBOSE} ${GCOVR_UNNECESSARY_ARCS} ${GCOVR_REMOVE_OPTIONS} ${GCOVR_SEARCH_PATHS}
+
+    DEPENDS buildcc test
 
     VERBATIM USES_TERMINAL
 )
