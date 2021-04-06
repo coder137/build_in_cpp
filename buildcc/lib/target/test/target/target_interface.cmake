a44b2b48a150a50311a43f68298777a2c163ea99
Niket Naidu <coder137@gmail.com>
2021-04-06 16:59:02 -0700

    Updated target test with mock env
    - Mocked out logging functions

--- buildcc/lib/target/test/target/target_interface.cmake
+++ buildcc/lib/target/test/target/target_interface.cmake
@@ -1,23 +1,23 @@
 add_library(target_interface INTERFACE)
 target_sources(target_interface INTERFACE
-    ${TARGET_DIR}/src/target.cpp
-    ${TARGET_DIR}/src/toolchain.cpp
+    ${target_SOURCE_DIR}/src/target.cpp
+    ${target_SOURCE_DIR}/src/toolchain.cpp
 
-    ${TARGET_DIR}/src/fbs_loader.cpp
-    ${TARGET_DIR}/src/fbs_storer.cpp
+    ${target_SOURCE_DIR}/src/fbs_loader.cpp
+    ${target_SOURCE_DIR}/src/fbs_storer.cpp
 
-    ${TARGET_DIR}/src/util.cpp
+    ${target_SOURCE_DIR}/src/util.cpp
 
-    ${TARGET_DIR}/src/static_target.cpp
-    ${TARGET_DIR}/src/dynamic_target.cpp   
+    ${target_SOURCE_DIR}/src/static_target.cpp
+    ${target_SOURCE_DIR}/src/dynamic_target.cpp   
 )
 target_include_directories(target_interface INTERFACE
-    ${TARGET_DIR}/include
-    ${TARGET_DIR}/generated
+    ${target_SOURCE_DIR}/include
+    ${target_SOURCE_DIR}/generated
     ${CMAKE_CURRENT_BINARY_DIR}/generated
 )
 target_link_libraries(target_interface INTERFACE
-    env
+    mock_env
     flatbuffers
     CppUTest
     CppUTestExt
