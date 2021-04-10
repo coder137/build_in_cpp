a21eff1427d0100b6fa45eee129ec9c9ec91ca9e
Niket Naidu <coder137@gmail.com>
2021-04-09 20:07:36 -0700

    Updated static lib example

---
+++ example/StaticLib/build.cpp
@@ -0,0 +1,32 @@
+#include <filesystem>
+#include <iostream>
+
+#include "buildcc.h"
+#include "constants.h"
+
+using namespace buildcc;
+
+int main(void) {
+  // Environment is meant to define
+  // 1. Project Root path i.e all files and targets will be added relative to
+  // this path
+  // 2. Intermediate build folder i.e all intermediate generated files should go
+  // here
+  env::init(BUILD_ROOT, BUILD_INTERMEDIATE_DIR);
+  env::set_log_level(env::LogLevel::Trace);
+
+  base::Toolchain gcc("gcc", "as", "gcc", "g++", "ar", "ld");
+
+  StaticTarget randomLib("libran.a", gcc, "files");
+  randomLib.AddSource("src/random.cpp");
+  randomLib.AddIncludeDir("include");
+  randomLib.Build();
+
+  ExecutableTarget target("statictest.exe", gcc, "files");
+  target.AddSource("main.cpp", "src");
+  target.AddIncludeDir("include");
+  target.AddLibDep(randomLib);
+  target.Build();
+
+  return 0;
+}
---
+++ example/StaticLib/files/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "random.h"
+
+int main() {
+  std::cout << "Hello World from test" << std::endl;
+  random_print();
+  return 0;
+}
