9d57d72f158e1205fa576957619ebde50a61363d
Niket Naidu <coder137@gmail.com>
2021-04-10 17:38:39 -0700

    Updated target compile and link flags

--- cmake/flags/test_flags.cmake
+++ cmake/flags/test_flags.cmake
@@ -1,5 +1,4 @@
-set(TEST_COMPILE_FLAGS -g -Og -fprofile-arcs -ftest-coverage -fno-omit-frame-pointer -fno-optimize-sibling-calls
--fno-inline -fprofile-abs-path -fbranch-probabilities
+set(TEST_COMPILE_FLAGS -g -Og -fprofile-arcs -ftest-coverage -fno-omit-frame-pointer -fno-optimize-sibling-calls -fno-inline -fprofile-abs-path
 )
-set(TEST_LINK_FLAGS -g -Og -fprofile-arcs -ftest-coverage -fprofile-abs-path -fbranch-probabilities
+set(TEST_LINK_FLAGS -g -Og -fprofile-arcs -ftest-coverage -fprofile-abs-path
 )
