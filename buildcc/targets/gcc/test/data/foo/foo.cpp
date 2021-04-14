8e75127fcd1703ed22a0817fcc6d197c57a4d323
Niket Naidu <coder137@gmail.com>
2021-04-13 23:10:39 -0700

    Removed targets gcc test

--- buildcc/targets/gcc/test/data/foo/foo.cpp
+++
@@ -1,5 +0,0 @@
-#include "foo.h"
-
-#include <iostream>
-
-void vFoo() { std::cout << __FUNCTION__ << std::endl; }
--- buildcc/targets/gcc/test/data/foo_main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#include "foo.h"
-
-int main() {
-  std::cout << "hello world\r\n";
-  vFoo();
-  return 0;
-}
