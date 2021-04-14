58da9e71b49df5ed927becc5385cd817b9d56b36
Niket Naidu <coder137@gmail.com>
2021-04-13 20:02:04 -0700

    Added static library test
    Shifted dynamic library to gcc folder as well

---
+++ buildcc/targets/gcc/test/data/empty_main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World\r\n";
+  return 0;
+}
---
+++ buildcc/targets/gcc/test/data/foo_main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "foo.h"
+
+int main() {
+  std::cout << "hello world\r\n";
+  vFoo();
+  return 0;
+}
