90d12ffd870edb0fa594465cf8e2e82219355786
Niket Naidu <coder137@gmail.com>
2021-04-15 01:49:34 -0700

    Cleaned util aggregate apis

--- buildcc/lib/target/src/util/util.cpp
+++ buildcc/lib/target/src/util/util.cpp
@@ -2,34 +2,6 @@
 
 #include "env.h"
 
-namespace {
-
-std::string aggregate(const std::vector<std::string> &list) {
-  std::string agg = "";
-  for (const auto &l : list) {
-    agg += l + " ";
-  }
-  return agg;
-}
-
-std::string aggregate(const std::unordered_set<std::string> &list) {
-  std::string agg = "";
-  for (const auto &l : list) {
-    agg += l + " ";
-  }
-  return agg;
-}
-
-std::string aggregate(const buildcc::internal::path_unordered_set &paths) {
-  std::string agg = "";
-  for (const auto &p : paths) {
-    agg += p.GetPathname().string() + " ";
-  }
-  return agg;
-}
-
-} // namespace
-
 namespace buildcc::internal {
 
 // rechecks
@@ -54,28 +26,28 @@ std::string aggregate_include_dirs(
   return idir;
 }
 
-std::string
-aggregate_compiled_sources(const std::vector<std::string> &compiled_sources) {
-  return aggregate(compiled_sources);
+std::string aggregate(const std::vector<std::string> &list) {
+  std::string agg = "";
+  for (const auto &l : list) {
+    agg += l + " ";
+  }
+  return agg;
 }
 
-std::string aggregate_preprocessor_flags(
-    const std::unordered_set<std::string> &preprocessor_flags) {
-  return aggregate(preprocessor_flags);
+std::string aggregate(const std::unordered_set<std::string> &list) {
+  std::string agg = "";
+  for (const auto &l : list) {
+    agg += l + " ";
+  }
+  return agg;
 }
 
-std::string
-aggregate_compile_flags(const std::unordered_set<std::string> &compile_flags) {
-  return aggregate(compile_flags);
-}
-
-std::string
-aggregate_link_flags(const std::unordered_set<std::string> &compiled_sources) {
-  return aggregate(compiled_sources);
-}
-
-std::string aggregate_lib_deps(const path_unordered_set &lib_deps) {
-  return aggregate(lib_deps);
+std::string aggregate(const buildcc::internal::path_unordered_set &paths) {
+  std::string agg = "";
+  for (const auto &p : paths) {
+    agg += p.GetPathname().string() + " ";
+  }
+  return agg;
 }
 
 } // namespace buildcc::internal
