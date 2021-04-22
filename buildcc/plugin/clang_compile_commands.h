7b951d0ef29e5f2d940789239ca50a6c3714a1aa
Niket Naidu <coder137@gmail.com>
2021-04-22 02:58:11 -0700

    Added plugin clang_compile_commands

---
+++ buildcc/plugin/clang_compile_commands.h
@@ -0,0 +1,24 @@
+#ifndef PLUGIN_CLANG_COMPILE_COMMANDS_H_
+#define PLUGIN_CLANG_COMPILE_COMMANDS_H_
+
+#include "target.h"
+
+namespace buildcc::plugin {
+
+class ClangCompileCommands {
+public:
+  ClangCompileCommands(std::vector<const base::Target *> targets)
+      : targets_(std::move(targets)) {}
+  ClangCompileCommands(const ClangCompileCommands &compile_commands) = delete;
+
+  void AddTarget(const base::Target *target);
+
+  void Generate();
+
+private:
+  std::vector<const base::Target *> targets_;
+};
+
+} // namespace buildcc::plugin
+
+#endif
