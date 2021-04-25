765562e06e73f87e49872933e15a38463635e0f5
Niket Naidu <coder137@gmail.com>
2021-04-24 19:52:59 -0700

    Added installation target for toolchain_specialized INTERFACE library
    - Updated buildcc.h include file

--- buildcc/include/buildcc.h
+++ buildcc/include/buildcc.h
@@ -1,18 +1,26 @@
 #ifndef BUILDCC_INCLUDE_BUILDCC_H_
 #define BUILDCC_INCLUDE_BUILDCC_H_
 
+// clang-format off
+
 // Core persistent environment
 #include "env.h"
 #include "logging.h"
 
-// Tested with `gcc and g++`
-#include "dynamic_target_gcc.h"
+// Base
+#include "toolchain.h"
+#include "target.h"
+
+// Toolchain
+#include "toolchain_gcc.h"
+
+// TODO, Add more specialized toolchains here
+
+// Target
 #include "executable_target_gcc.h"
 #include "static_target_gcc.h"
+#include "dynamic_target_gcc.h"
 
-// TODO, Add 'compiler specific' target headers here for
-// Executable Target
-// Static Target
-// Dynamic Target
+// TODO, Add more specialized targets here
 
 #endif
