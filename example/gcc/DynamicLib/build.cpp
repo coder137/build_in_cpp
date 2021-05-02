18de0f8b3951dc6b8a37f16076fe44038741433f
Niket Naidu <coder137@gmail.com>
2021-05-01 18:34:33 -0700

    Updated DynamicLib example with both linux and windows oses
    - Uses MSYS 10.2.0 Windows
    - Uses GCC 9.3.0 Linux

--- example/gcc/DynamicLib/build.cpp
+++ example/gcc/DynamicLib/build.cpp
@@ -6,6 +6,10 @@
 
 using namespace buildcc;
 
+// This example contains both OS Hosts
+// - Windows MSYS GCC 10.2.0
+// - Linux GCC 9.3.0
+// * Comment out one of the examples
 int main(void) {
   // Environment is meant to define
   // 1. Project Root path i.e all files and targets will be added relative to
@@ -17,31 +21,59 @@ int main(void) {
 
   base::Toolchain gcc("gcc", "as", "gcc", "g++", "ar", "ld");
 
-  DynamicTarget_gcc randomDynLib("librandyn.dll", gcc, "files");
-  randomDynLib.AddSource("src/random.cpp");
-  randomDynLib.AddHeader("include/random.h");
-  randomDynLib.AddIncludeDir("include");
-  randomDynLib.Build();
+  // Linux GCC
+  {
+    DynamicTarget_gcc randomDynLib("librandyn.so", gcc, "files");
+    randomDynLib.AddSource("src/random.cpp");
+    randomDynLib.AddHeader("include/random.h");
+    randomDynLib.AddIncludeDir("include");
+    randomDynLib.Build();
 
-  ExecutableTarget_gcc target("dynamictest.exe", gcc, "files");
-  target.AddSource("main.cpp", "src");
-  target.AddIncludeDir("include");
+    ExecutableTarget_gcc target("dynamictest.exe", gcc, "files");
+    target.AddSource("main.cpp", "src");
+    target.AddIncludeDir("include");
 
-  // * Method 1
-  // NOTE, Use buildcc built targets
-  // target.AddLibDep(randomDynLib);
+    // * Method 1
+    // NOTE, Use buildcc built targets
+    // target.AddLibDep(randomDynLib);
 
-  // * Method 2, External lib
-  target.AddLibDir(randomDynLib.GetTargetIntermediateDir());
-  target.AddLibDep("-lrandyn");
+    // * Method 2, External lib
+    target.AddLibDir(randomDynLib.GetTargetIntermediateDir());
+    target.AddLibDep("-lrandyn");
+    target.AddLinkFlag("-Wl,-rpath=" +
+                       randomDynLib.GetTargetIntermediateDir().string());
 
-  target.Build();
+    target.Build();
+  }
 
-  if (target.FirstBuild() || target.Rebuild()) {
-    fs::path copy_to_path =
-        target.GetTargetIntermediateDir() / randomDynLib.GetName();
-    fs::remove(copy_to_path);
-    fs::copy(randomDynLib.GetTargetPath(), copy_to_path);
+  // MingW GCC
+  {
+    DynamicTarget_gcc randomDynLib("librandyn.dll", gcc, "files");
+    randomDynLib.AddSource("src/random.cpp");
+    randomDynLib.AddHeader("include/random.h");
+    randomDynLib.AddIncludeDir("include");
+    randomDynLib.Build();
+
+    ExecutableTarget_gcc target("dynamictest.exe", gcc, "files");
+    target.AddSource("main.cpp", "src");
+    target.AddIncludeDir("include");
+
+    // * Method 1
+    // NOTE, Use buildcc built targets
+    // target.AddLibDep(randomDynLib);
+
+    // * Method 2, External lib
+    target.AddLibDir(randomDynLib.GetTargetIntermediateDir());
+    target.AddLibDep("-lrandyn");
+
+    target.Build();
+
+    if (target.FirstBuild() || target.Rebuild()) {
+      fs::path copy_to_path =
+          target.GetTargetIntermediateDir() / randomDynLib.GetName();
+      fs::remove(copy_to_path);
+      fs::copy(randomDynLib.GetTargetPath(), copy_to_path);
+    }
   }
 
   return 0;
