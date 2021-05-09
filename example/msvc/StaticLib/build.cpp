69c1fcfd90aad76698d1b9da352379b4f306cd27
Niket Naidu <coder137@gmail.com>
2021-05-08 19:26:49 -0700

    Added StaticLib example for MSVC

---
+++ example/msvc/StaticLib/build.cpp
@@ -0,0 +1,50 @@
+#include <filesystem>
+#include <iostream>
+
+#include "buildcc.h"
+#include "constants.h"
+
+#include "clang_compile_commands.h"
+
+using namespace buildcc;
+
+int main(void) {
+  // Environment is meant to define
+  // 1. Project Root path i.e all files and targets will be added relative to
+  // this path
+  // 2. Intermediate build folder i.e all intermediate generated files should go
+  // here
+  env::init(BUILD_SCRIPT_SOURCE, BUILD_SCRIPT_FOLDER);
+  env::set_log_level(env::LogLevel::Trace);
+
+  // base::Toolchain msvc("msvc", "cl", "cl", "cl", "lib", "link");
+  Toolchain_msvc msvc;
+
+  StaticTarget_msvc statictarget_msvc("librandom.lib", msvc, "");
+  statictarget_msvc.AddSource("src/random.cpp");
+  statictarget_msvc.AddIncludeDir("include", true);
+  statictarget_msvc.AddCppCompileFlag("/EHsc");
+  statictarget_msvc.AddCppCompileFlag("/nologo");
+  statictarget_msvc.AddLinkFlag("/nologo");
+  statictarget_msvc.Build();
+
+  ExecutableTarget_msvc target_msvc("Simple.exe", msvc, "");
+  target_msvc.AddSource("src/main.cpp");
+  target_msvc.AddIncludeDir("include", true);
+
+  // Method 1
+  // target_msvc.AddLibDep(statictarget_msvc);
+
+  // Method 2
+  target_msvc.AddLibDep("librandom.lib");
+  target_msvc.AddLibDir(statictarget_msvc.GetTargetPath().parent_path());
+
+  target_msvc.AddCppCompileFlag("/EHsc");
+  target_msvc.AddCppCompileFlag("/nologo");
+  target_msvc.AddLinkFlag("/nologo");
+  target_msvc.Build();
+
+  plugin::ClangCompileCommands({&target_msvc}).Generate();
+
+  return 0;
+}
