b2c90db22f8b151e053e9213011ae82d9d1069f3
Niket Naidu <coder137@gmail.com>
2021-05-26 23:00:30 -0700

    Updated mocks to conform with unit tests

--- buildcc/lib/target/mock/target/tasks.cpp
+++ buildcc/lib/target/mock/target/tasks.cpp
@@ -4,49 +4,14 @@
 
 namespace buildcc::base {
 
-static constexpr const char *const COMPILETARGETTASK_FUNCTION =
-    "Target::CompileTargetTask";
-static constexpr const char *const COMPILETARGETTASK_COMPILESOURCES_PARAM =
-    "compile_sources";
-static constexpr const char *const COMPILETARGETTASK_DUMMYCOMPILESOURCES_PARAM =
-    "dummy_compile_sources";
-
-static constexpr const char *const LINKTARGETTASK_FUNCTION =
-    "Target::LinkTargetTask";
-
 void Target::CompileTargetTask(
     const std::vector<fs::path> &&compile_sources,
     const std::vector<fs::path> &&dummy_compile_sources) {
-  mock()
-      .actualCall(COMPILETARGETTASK_FUNCTION)
-      .onObject(this)
-      .withIntParameter(COMPILETARGETTASK_COMPILESOURCES_PARAM,
-                        compile_sources.size())
-      .withIntParameter(COMPILETARGETTASK_DUMMYCOMPILESOURCES_PARAM,
-                        dummy_compile_sources.size());
+  for (const auto &cs : compile_sources) {
+    CompileSource(cs);
+  }
 }
 
-void Target::LinkTargetTask() {
-  mock().actualCall(LINKTARGETTASK_FUNCTION).onObject(this);
-}
-
-namespace m {
-
-void TargetExpect_CompileTargetTask(unsigned int calls, Target *target,
-                                    int compile_sources,
-                                    int dummy_compile_sources) {
-  mock()
-      .expectNCalls(calls, COMPILETARGETTASK_FUNCTION)
-      .onObject(target)
-      .withIntParameter(COMPILETARGETTASK_COMPILESOURCES_PARAM, compile_sources)
-      .withIntParameter(COMPILETARGETTASK_DUMMYCOMPILESOURCES_PARAM,
-                        dummy_compile_sources);
-}
-
-void TargetExpect_LinkTargetTask(unsigned int calls, Target *target) {
-  mock().expectNCalls(calls, LINKTARGETTASK_FUNCTION).onObject(target);
-}
-
-} // namespace m
+void Target::LinkTargetTask() { LinkTarget(); }
 
 } // namespace buildcc::base
