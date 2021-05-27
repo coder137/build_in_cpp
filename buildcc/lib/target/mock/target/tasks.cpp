c3157d242c4e1ccdc238b0279668b9320d43a750
Niket Naidu <coder137@gmail.com>
2021-05-27 04:27:35 -0700

    Updated target task mock with unused dummy_compile_sources

--- buildcc/lib/target/mock/target/tasks.cpp
+++ buildcc/lib/target/mock/target/tasks.cpp
@@ -7,6 +7,7 @@ namespace buildcc::base {
 void Target::CompileTargetTask(
     const std::vector<fs::path> &&compile_sources,
     const std::vector<fs::path> &&dummy_compile_sources) {
+  (void)dummy_compile_sources;
   for (const auto &cs : compile_sources) {
     CompileSource(cs);
   }
