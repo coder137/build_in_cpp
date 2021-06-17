15a2fdb92fe491f64799e0b7a8bd4cfb179d8fdc
Niket Naidu <coder137@gmail.com>
2021-06-16 23:07:37 -0700

    Target flatbuffer schema update (#64)
    Do not track Toolchain and relative_path in Flatbuffer schema
    Updated fbs_storer with the updated schema
    Updated fbs_loader with verifier

--- buildcc/lib/target/src/fbs/fbs_loader.cpp
+++ buildcc/lib/target/src/fbs/fbs_loader.cpp
@@ -67,11 +67,18 @@ bool FbsLoader::Load() {
   if (!is_loaded) {
     return false;
   }
+
+  flatbuffers::Verifier verifier((const uint8_t *)buffer.c_str(),
+                                 buffer.length());
+  const bool is_verified = fbs::VerifyTargetBuffer(verifier);
+  if (!is_verified) {
+    return false;
+  }
+
   const auto *target = fbs::GetTarget((const void *)buffer.c_str());
   // target->name()->c_str();
-  // target->relative_path()->c_str();
   // target->type();
-  // target->toolchain();
+
   Extract(target->source_files(), loaded_sources_);
   Extract(target->header_files(), loaded_headers_);
   Extract(target->lib_deps(), loaded_lib_deps_);
