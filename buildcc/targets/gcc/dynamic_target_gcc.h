b2f2fae96f47d5ca69d6a2cd387f51ff8f48ca62
Niket Naidu <coder137@gmail.com>
2021-06-17 11:19:08 -0700

    Updated gcc

--- buildcc/targets/gcc/dynamic_target_gcc.h
+++ buildcc/targets/gcc/dynamic_target_gcc.h
@@ -29,21 +29,13 @@ public:
                target_path_relative_to_root) {}
 
 private:
-  // Compiling
-  virtual std::vector<std::string>
-  CompileCommand(const std::string &input_source,
-                 const std::string &output_source, const std::string &compiler,
-                 const std::string &aggregated_preprocessor_flags,
-                 const std::string &aggregated_compile_flags,
-                 const std::string &aggregated_include_dirs) const override;
-
-  // Linking
-  virtual std::vector<std::string>
-  Link(const std::string &output_target,
-       const std::string &aggregated_link_flags,
-       const std::string &aggregated_compiled_sources,
-       const std::string &aggregated_lib_dirs,
-       const std::string &aggregated_lib_deps) const override;
+  virtual std::string_view CompileCommand() const {
+    return "{compiler} {preprocessor_flags} {include_dirs} {compile_flags} "
+           "-fpic -o {output} -c {input}";
+  }
+  virtual std::string_view Link() const {
+    return "{cpp_compiler} -shared {link_flags} {compiled_sources} -o {output}";
+  }
 };
 
 } // namespace buildcc
--- buildcc/targets/gcc/static_target_gcc.h
+++ buildcc/targets/gcc/static_target_gcc.h
@@ -29,14 +29,9 @@ public:
                target_path_relative_to_root) {}
 
 private:
-  // NOTE: Compiling does not need to change
-
-  virtual std::vector<std::string>
-  Link(const std::string &output_target,
-       const std::string &aggregated_link_flags,
-       const std::string &aggregated_compiled_sources,
-       const std::string &aggregated_lib_dirs,
-       const std::string &aggregated_lib_deps) const override;
+  virtual std::string_view Link() const {
+    return "{archiver} rcs {output} {compiled_sources}";
+  }
 };
 
 } // namespace buildcc
