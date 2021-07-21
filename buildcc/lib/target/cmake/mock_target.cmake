940d53fe92d001ca6af077597673ef14a6988136
Niket Naidu <coder137@gmail.com>
2021-07-21 01:45:27 -0700

    Converted mock_env from PUBLIC to PRIVATE to reduce compilation

--- buildcc/lib/target/cmake/mock_target.cmake
+++ buildcc/lib/target/cmake/mock_target.cmake
@@ -1,26 +1,24 @@
-add_library(mock_target STATIC)
+add_library(mock_target STATIC
+    src/target/target.cpp
+    src/target/source.cpp
+    src/target/include_dir.cpp
+    src/target/lib.cpp
+    src/target/build.cpp
+    src/target/flags.cpp
+    mock/target/recheck_states.cpp
+    mock/target/tasks.cpp
+
+    src/fbs/fbs_loader.cpp
+    src/fbs/fbs_storer.cpp
+
+    src/util/util.cpp
+    src/util/command.cpp
+    mock/util/execute.cpp
+)
 target_include_directories(mock_target PUBLIC
     ${CMAKE_CURRENT_SOURCE_DIR}/include
     ${CMAKE_CURRENT_SOURCE_DIR}/mock
     ${CMAKE_CURRENT_BINARY_DIR}/generated
-)
-
-target_sources(mock_target PRIVATE
-    ${CMAKE_CURRENT_SOURCE_DIR}/src/target/target.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/src/target/source.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/src/target/include_dir.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/src/target/lib.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/src/target/build.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/src/target/flags.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/mock/target/recheck_states.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/mock/target/tasks.cpp
-
-    ${CMAKE_CURRENT_SOURCE_DIR}/src/fbs/fbs_loader.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/src/fbs/fbs_storer.cpp
-
-    ${CMAKE_CURRENT_SOURCE_DIR}/src/util/util.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/src/util/command.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/mock/util/execute.cpp
 )
 
 target_compile_options(mock_target PUBLIC ${TEST_COMPILE_FLAGS} ${BUILD_COMPILE_FLAGS})
