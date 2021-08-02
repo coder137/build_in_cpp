cff13a9a0b6c2b99ae12d7f2d479c803feb71258
Niket Naidu <coder137@gmail.com>
2021-08-02 02:01:48 -0700

    Buildcc Single and Interface Library segregation (#82)

--- buildcc/lib/target/cmake/target.cmake
+++ buildcc/lib/target/cmake/target.cmake
@@ -1,6 +1,4 @@
-# Target
-m_clangtidy("target")
-add_library(target
+set(TARGET_SRCS 
     src/target/target.cpp
     src/target/source.cpp
     src/target/include_dir.cpp
@@ -24,23 +22,44 @@ add_library(target
     include/target/util.h
     include/target/command.h
 )
-target_include_directories(target PUBLIC
-    $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
-    $<INSTALL_INTERFACE:${BUILDCC_INSTALL_HEADER_PREFIX}>
-)
-target_link_libraries(target PUBLIC 
-    env
-    toolchain
-    flatbuffers_header_only
-    Taskflow
-)
-target_link_libraries(target PRIVATE
-    tiny-process-library::tiny-process-library
-)
 
-target_include_directories(target PRIVATE
-   ${CMAKE_CURRENT_BINARY_DIR}/generated
-)
-target_compile_options(target PRIVATE ${BUILD_COMPILE_FLAGS})
-target_link_options(target PRIVATE ${BUILD_LINK_FLAGS})
-add_dependencies(target fbs_to_header)
+if(${BUILDCC_BUILD_AS_SINGLE_LIB})
+    target_sources(buildcc PRIVATE
+        ${TARGET_SRCS}
+    )
+    target_include_directories(buildcc PUBLIC
+        $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
+        $<INSTALL_INTERFACE:${BUILDCC_INSTALL_HEADER_PREFIX}>
+    )
+    target_include_directories(buildcc PRIVATE
+        ${CMAKE_CURRENT_BINARY_DIR}/generated
+    )
+    add_dependencies(buildcc fbs_to_header)
+endif()
+
+if(${BUILDCC_BUILD_AS_INTERFACE})
+    m_clangtidy("target")
+    add_library(target
+        ${TARGET_SRCS}
+    )
+    target_include_directories(target PUBLIC
+        $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
+        $<INSTALL_INTERFACE:${BUILDCC_INSTALL_HEADER_PREFIX}>
+    )
+    target_link_libraries(target PUBLIC 
+        env
+        toolchain
+        flatbuffers_header_only
+        Taskflow
+    )
+    target_link_libraries(target PRIVATE
+        tiny-process-library::tiny-process-library
+    )
+
+    target_include_directories(target PRIVATE
+        ${CMAKE_CURRENT_BINARY_DIR}/generated
+    )
+    target_compile_options(target PRIVATE ${BUILD_COMPILE_FLAGS})
+    target_link_options(target PRIVATE ${BUILD_LINK_FLAGS})
+    add_dependencies(target fbs_to_header)
+endif()
