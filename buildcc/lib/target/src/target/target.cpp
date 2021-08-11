49bbbc1606200176b76f99134439efe82bda8961
Niket Naidu <coder137@gmail.com>
2021-08-10 21:23:20 -0700

    Updated Taskflow name

--- buildcc/lib/target/src/target/target.cpp
+++ buildcc/lib/target/src/target/target.cpp
@@ -163,9 +163,6 @@ void Target::Initialize() {
       "Environment is not initialized. Use the buildcc::env::init API");
   env::assert_fatal(IsValidTargetType(type_), "Invalid Target Type");
   fs::create_directories(target_intermediate_dir_);
-
-  // Taskflow parameters
-  tf_.name(name_);
 }
 
 // Rechecks
