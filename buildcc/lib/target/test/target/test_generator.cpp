9785d6fc4c8a40dd77b1c1e78bbe15cec8efe4b9
Niket Naidu <coder137@gmail.com>
2021-08-29 14:56:13 -0700

    Update test_generator.cpp

--- buildcc/lib/target/test/target/test_generator.cpp
+++ buildcc/lib/target/test/target/test_generator.cpp
@@ -100,7 +100,56 @@ TEST(GeneratorTestGroup, Generator_Rebuild) {
   mock().checkExpectations();
 }
 
-TEST(GeneratorTestGroup, Generator_Rebuild_PreviousNotFound) {}
+TEST(GeneratorTestGroup, Generator_Rebuild_PreviousNotFound) {
+  fs::path TEST_BUILD_DIR = BUILD_DIR / "Rebuild_PreviousNotFound";
+  fs::create_directories(TEST_BUILD_DIR);
+
+  {
+    buildcc::base::Generator generator("custom_file_generator", TEST_BUILD_DIR);
+    generator.AddGenInfo(buildcc::base::UserGenInfo(
+        "gcc_1",
+        {
+            "data/dummy_main.c",
+        },
+        {TEST_BUILD_DIR / "dummy_main.exe"},
+        {"gcc -o intermediate/generator/AddInfo/dummy_main.exe "
+         "data/dummy_main.c"},
+        true));
+
+    buildcc::m::CommandExpect_Execute(1, true);
+    generator.Build();
+  }
+
+  {
+    buildcc::base::Generator generator("custom_file_generator", TEST_BUILD_DIR);
+    generator.AddGenInfo(buildcc::base::UserGenInfo(
+        "gcc_1",
+        {
+            "data/dummy_main.c",
+        },
+        {TEST_BUILD_DIR / "dummy_main.exe"},
+        {"gcc -o intermediate/generator/AddInfo/dummy_main.exe "
+         "data/dummy_main.c"},
+        true));
+
+    // Current Info is NEWER than old
+    generator.AddGenInfo(buildcc::base::UserGenInfo(
+        "gcc_2",
+        {
+            "data/dummy_main.c",
+        },
+        {TEST_BUILD_DIR / "dummy_main.exe"},
+        {"gcc -o intermediate/generator/AddInfo/dummy_main.exe "
+         "data/dummy_main.c"},
+        true));
+
+    buildcc::m::CommandExpect_Execute(1, true);
+    generator.Build();
+  }
+
+  mock().checkExpectations();
+}
+
 TEST(GeneratorTestGroup, Generator_Rebuild_Remove) {}
 TEST(GeneratorTestGroup, Generator_Rebuild_Add) {}
 TEST(GeneratorTestGroup, Generator_Rebuild_Update) {}
