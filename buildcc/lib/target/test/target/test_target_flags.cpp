62890bc5ba2092e14d0dc1cfbddb7d120647e1ac
Niket Naidu <coder137@gmail.com>
2021-09-18 20:17:32 -0700

    Added linkflags to test target_flags

--- buildcc/lib/target/test/target/test_target_flags.cpp
+++ buildcc/lib/target/test/target/test_target_flags.cpp
@@ -292,6 +292,93 @@ TEST(TargetTestCppCompileFlagsGroup, Target_ChangedCompileFlag) {
   mock().checkExpectations();
 }
 
+// ------------- LINK FLAGS ---------------
+
+// clang-format off
+TEST_GROUP(TargetTestLinkFlagsGroup)
+{
+    void teardown() {
+      mock().clear();
+    }
+};
+// clang-format on
+
+static const fs::path target_linkflag_intermediate_path =
+    fs::path(BUILD_TARGET_FLAG_INTERMEDIATE_DIR) / gcc.GetName();
+
+TEST(TargetTestLinkFlagsGroup, Target_AddLinkFlag) {
+  constexpr const char *const NAME = "AddLinkFlag.exe";
+  constexpr const char *const DUMMY_MAIN = "dummy_main.cpp";
+
+  auto source_path = fs::path(BUILD_SCRIPT_SOURCE) / "data";
+  auto intermediate_path = target_linkflag_intermediate_path / NAME;
+
+  // Delete
+  fs::remove_all(intermediate_path);
+
+  buildcc::base::Target simple(NAME, buildcc::base::TargetType::Executable, gcc,
+                               "data");
+  simple.AddSource(DUMMY_MAIN);
+  simple.AddLinkFlag("-lm");
+
+  buildcc::m::CommandExpect_Execute(1, true);
+  buildcc::m::CommandExpect_Execute(1, true);
+  simple.Build();
+
+  mock().checkExpectations();
+
+  // Verify binary
+  buildcc::internal::FbsLoader loader(NAME, simple.GetTargetIntermediateDir());
+  bool loaded = loader.Load();
+  CHECK_TRUE(loaded);
+
+  CHECK_EQUAL(loader.GetLoadedLinkFlags().size(), 1);
+}
+
+TEST(TargetTestLinkFlagsGroup, Target_ChangedLinkFlag) {
+  constexpr const char *const NAME = "ChangedLinkFlag.exe";
+  constexpr const char *const DUMMY_MAIN = "dummy_main.cpp";
+
+  auto source_path = fs::path(BUILD_SCRIPT_SOURCE) / "data";
+  auto intermediate_path = target_linkflag_intermediate_path / NAME;
+
+  // Delete
+  fs::remove_all(intermediate_path);
+
+  {
+    buildcc::base::Target simple(NAME, buildcc::base::TargetType::Executable,
+                                 gcc, "data");
+    simple.AddSource(DUMMY_MAIN);
+    simple.AddLinkFlag("-lm");
+
+    buildcc::m::CommandExpect_Execute(1, true);
+    buildcc::m::CommandExpect_Execute(1, true);
+    simple.Build();
+  }
+  {
+    // * Remove flag
+    buildcc::base::Target simple(NAME, buildcc::base::TargetType::Executable,
+                                 gcc, "data");
+    simple.AddSource(DUMMY_MAIN);
+    buildcc::base::m::TargetExpect_FlagChanged(1, &simple);
+    buildcc::m::CommandExpect_Execute(1, true);
+    simple.Build();
+  }
+
+  {
+    // * Add flag
+    buildcc::base::Target simple(NAME, buildcc::base::TargetType::Executable,
+                                 gcc, "data");
+    simple.AddSource(DUMMY_MAIN);
+    simple.AddLinkFlag("-lm");
+    buildcc::base::m::TargetExpect_FlagChanged(1, &simple);
+    buildcc::m::CommandExpect_Execute(1, true);
+    simple.Build();
+  }
+
+  mock().checkExpectations();
+}
+
 int main(int ac, char **av) {
   buildcc::env::init(BUILD_SCRIPT_SOURCE, BUILD_TARGET_FLAG_INTERMEDIATE_DIR);
   return CommandLineTestRunner::RunAllTests(ac, av);
