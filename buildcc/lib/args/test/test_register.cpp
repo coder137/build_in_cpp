cf1af8ecad8a63988120e30288d6ec63ba88282b
Niket Naidu <coder137@gmail.com>
2021-09-24 01:48:00 -0700

    Updated test_register with clean

--- buildcc/lib/args/test/test_register.cpp
+++ buildcc/lib/args/test/test_register.cpp
@@ -31,6 +31,51 @@ TEST(RegisterTestGroup, Register_Initialize) {
   buildcc::Register reg(args);
 }
 
+TEST(RegisterTestGroup, Register_Clean) {
+  {
+    std::vector<const char *> av{"", "--config", "configs/basic_parse.toml"};
+    int argc = av.size();
+
+    buildcc::Args args;
+    args.Parse(argc, av.data());
+
+    STRCMP_EQUAL(args.GetProjectRootDir().string().c_str(), "root");
+    STRCMP_EQUAL(args.GetProjectBuildDir().string().c_str(), "build");
+    CHECK(args.GetLogLevel() == buildcc::env::LogLevel::Trace);
+    CHECK_TRUE(args.Clean());
+
+    buildcc::Register reg(args);
+    mock().expectOneCall("CleanCb");
+    reg.Clean([]() { mock().actualCall("CleanCb"); });
+    mock().checkExpectations();
+  }
+
+  {
+    {
+      std::vector<const char *> av{
+          "",
+          "--config",
+          "configs/basic_parse.toml",
+          "--config",
+          "configs/no_clean.toml",
+      };
+      int argc = av.size();
+
+      buildcc::Args args;
+      args.Parse(argc, av.data());
+
+      STRCMP_EQUAL(args.GetProjectRootDir().string().c_str(), "root");
+      STRCMP_EQUAL(args.GetProjectBuildDir().string().c_str(), "build");
+      CHECK(args.GetLogLevel() == buildcc::env::LogLevel::Trace);
+      CHECK_FALSE(args.Clean());
+
+      buildcc::Register reg(args);
+      reg.Clean([]() { mock().actualCall("CleanCb"); });
+      mock().checkExpectations();
+    }
+  }
+}
+
 int main(int ac, char **av) {
   return CommandLineTestRunner::RunAllTests(ac, av);
 }
