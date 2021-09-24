0ff47ad99b388efaac791458f7d26e08e2ad1947
Niket Naidu <coder137@gmail.com>
2021-09-23 20:22:22 -0700

    Updated test_arg with gcc custom toolchain

--- buildcc/lib/args/test/test_args.cpp
+++ buildcc/lib/args/test/test_args.cpp
@@ -48,6 +48,37 @@ TEST(ArgsTestGroup, Args_MultiToml) {
   CHECK_FALSE(args.Clean());
 }
 
+TEST(ArgsTestGroup, Args_CustomToolchain) {
+  std::vector<const char *> av{"", "--config", "configs/basic_parse.toml",
+                               "--config", "configs/gcc_toolchain.toml"};
+  int argc = av.size();
+
+  buildcc::Args args;
+  buildcc::Args::ToolchainArg gcc_toolchain;
+  args.AddCustomToolchain("gcc", "Generic gcc toolchain", gcc_toolchain);
+  args.Parse(argc, av.data());
+
+  STRCMP_EQUAL(args.GetProjectRootDir().string().c_str(), "root");
+  STRCMP_EQUAL(args.GetProjectBuildDir().string().c_str(), "build");
+  CHECK(args.GetLogLevel() == buildcc::env::LogLevel::Trace);
+  CHECK_TRUE(args.Clean());
+
+  // Toolchain
+  CHECK_TRUE(gcc_toolchain.state.build);
+  CHECK_FALSE(gcc_toolchain.state.test);
+  CHECK(gcc_toolchain.id == buildcc::base::Toolchain::Id::Gcc);
+  STRCMP_EQUAL(gcc_toolchain.name.c_str(), "gcc");
+  STRCMP_EQUAL(gcc_toolchain.asm_compiler.c_str(), "as");
+  STRCMP_EQUAL(gcc_toolchain.c_compiler.c_str(), "gcc");
+  STRCMP_EQUAL(gcc_toolchain.cpp_compiler.c_str(), "g++");
+  STRCMP_EQUAL(gcc_toolchain.archiver.c_str(), "ar");
+  STRCMP_EQUAL(gcc_toolchain.linker.c_str(), "ld");
+}
+
+TEST(ArgsTestGroup, Args_MultipleCustomToolchain) {}
+
+TEST(ArgsTestGroup, Args_DuplicateCustomToolchain) {}
+
 int main(int ac, char **av) {
   return CommandLineTestRunner::RunAllTests(ac, av);
 }
