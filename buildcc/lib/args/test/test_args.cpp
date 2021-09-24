f3a4a7f8866631a7e8fb49a4f42f8ea7dfe22b63
Niket Naidu <coder137@gmail.com>
2021-09-23 20:26:21 -0700

    Updated test_args with multiple toolchains
    - GCC and MSVC added together

--- buildcc/lib/args/test/test_args.cpp
+++ buildcc/lib/args/test/test_args.cpp
@@ -75,7 +75,54 @@ TEST(ArgsTestGroup, Args_CustomToolchain) {
   STRCMP_EQUAL(gcc_toolchain.linker.c_str(), "ld");
 }
 
-TEST(ArgsTestGroup, Args_MultipleCustomToolchain) {}
+TEST(ArgsTestGroup, Args_MultipleCustomToolchain) {
+  std::vector<const char *> av{
+      "",
+      "--config",
+      "configs/basic_parse.toml",
+      "--config",
+      "configs/gcc_toolchain.toml",
+      "--config",
+      "configs/msvc_toolchain.toml",
+  };
+  int argc = av.size();
+
+  buildcc::Args args;
+  buildcc::Args::ToolchainArg gcc_toolchain;
+  buildcc::Args::ToolchainArg msvc_toolchain;
+  args.AddCustomToolchain("gcc", "Generic gcc toolchain", gcc_toolchain);
+  args.AddCustomToolchain("msvc", "Generic msvc toolchain", msvc_toolchain);
+  args.Parse(argc, av.data());
+
+  STRCMP_EQUAL(args.GetProjectRootDir().string().c_str(), "root");
+  STRCMP_EQUAL(args.GetProjectBuildDir().string().c_str(), "build");
+  CHECK(args.GetLogLevel() == buildcc::env::LogLevel::Trace);
+  CHECK_TRUE(args.Clean());
+
+  // Toolchain
+
+  // GCC
+  CHECK_TRUE(gcc_toolchain.state.build);
+  CHECK_FALSE(gcc_toolchain.state.test);
+  CHECK(gcc_toolchain.id == buildcc::base::Toolchain::Id::Gcc);
+  STRCMP_EQUAL(gcc_toolchain.name.c_str(), "gcc");
+  STRCMP_EQUAL(gcc_toolchain.asm_compiler.c_str(), "as");
+  STRCMP_EQUAL(gcc_toolchain.c_compiler.c_str(), "gcc");
+  STRCMP_EQUAL(gcc_toolchain.cpp_compiler.c_str(), "g++");
+  STRCMP_EQUAL(gcc_toolchain.archiver.c_str(), "ar");
+  STRCMP_EQUAL(gcc_toolchain.linker.c_str(), "ld");
+
+  // MSVC
+  CHECK_TRUE(msvc_toolchain.state.build);
+  CHECK_TRUE(msvc_toolchain.state.test);
+  CHECK(msvc_toolchain.id == buildcc::base::Toolchain::Id::Msvc);
+  STRCMP_EQUAL(msvc_toolchain.name.c_str(), "msvc");
+  STRCMP_EQUAL(msvc_toolchain.asm_compiler.c_str(), "cl");
+  STRCMP_EQUAL(msvc_toolchain.c_compiler.c_str(), "cl");
+  STRCMP_EQUAL(msvc_toolchain.cpp_compiler.c_str(), "cl");
+  STRCMP_EQUAL(msvc_toolchain.archiver.c_str(), "lib");
+  STRCMP_EQUAL(msvc_toolchain.linker.c_str(), "link");
+}
 
 TEST(ArgsTestGroup, Args_DuplicateCustomToolchain) {}
 
