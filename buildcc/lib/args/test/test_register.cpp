4b6258e0d24dae2f94f58ca63a0a90a125ff9d2a
Niket Naidu <coder137@gmail.com>
2021-09-25 01:31:57 -0700

    Added Register_Test test

--- buildcc/lib/args/test/test_register.cpp
+++ buildcc/lib/args/test/test_register.cpp
@@ -213,6 +213,89 @@ TEST(RegisterTestGroup, Register_Dep) {
   mock().checkExpectations();
 }
 
+TEST(RegisterTestGroup, Register_Test) {
+  // Arguments
+  std::vector<const char *> av{
+      "",
+      "--config",
+      "configs/basic_parse.toml",
+  };
+  int argc = av.size();
+
+  buildcc::Args args;
+  buildcc::Args::ToolchainArg gcc_toolchain;
+  buildcc::Args::ToolchainArg msvc_toolchain;
+  args.AddToolchain("gcc", "Generic gcc toolchain", gcc_toolchain);
+  args.AddToolchain("msvc", "Generic msvc toolchain", msvc_toolchain);
+  args.Parse(argc, av.data());
+
+  STRCMP_EQUAL(args.GetProjectRootDir().string().c_str(), "root");
+  STRCMP_EQUAL(args.GetProjectBuildDir().string().c_str(), "build");
+  CHECK(args.GetLogLevel() == buildcc::env::LogLevel::Trace);
+  CHECK_TRUE(args.Clean());
+
+  // Make dummy toolchain and target
+  buildcc::env::init(fs::current_path(), fs::current_path());
+  buildcc::base::Toolchain toolchain(buildcc::base::Toolchain::Id::Gcc, "", "",
+                                     "", "", "", "");
+  buildcc::base::Target target("dummyT", buildcc::base::TargetType::Executable,
+                               toolchain, "");
+  buildcc::base::Target dependency(
+      "depT", buildcc::base::TargetType::Executable, toolchain, "");
+
+  // 4 states between build and test
+  // FF
+  // TF
+  // FT
+  // TT -> only success case
+  buildcc::Args::ToolchainState stateFail{false, false};
+  buildcc::Args::ToolchainState state1{true, false};
+  buildcc::Args::ToolchainState state2{false, true};
+  buildcc::Args::ToolchainState stateSuccess{true, true};
+
+  // FF
+  {
+    buildcc::Register reg(args);
+    reg.Test(stateFail, target,
+             [](buildcc::base::Target &target) { (void)target; });
+  }
+
+  // TF
+  {
+    buildcc::Register reg(args);
+    reg.Test(state1, target,
+             [](buildcc::base::Target &target) { (void)target; });
+  }
+
+  // FT
+  {
+    buildcc::Register reg(args);
+    reg.Test(state2, target,
+             [](buildcc::base::Target &target) { (void)target; });
+  }
+
+  // TT
+  // Register::Build not called
+  {
+    buildcc::Register reg(args);
+    CHECK_THROWS(std::exception,
+                 reg.Test(stateSuccess, target,
+                          [](buildcc::base::Target &target) { (void)target; }));
+  }
+
+  {
+    buildcc::Register reg(args);
+    mock().expectNCalls(1, "BuildTask_dummyT");
+    reg.Build(stateSuccess, target,
+              [](buildcc::base::Target &target) { (void)target; });
+    reg.Test(stateSuccess, target,
+             [](buildcc::base::Target &target) { (void)target; });
+  }
+
+  buildcc::env::deinit();
+  mock().checkExpectations();
+}
+
 int main(int ac, char **av) {
   return CommandLineTestRunner::RunAllTests(ac, av);
 }
