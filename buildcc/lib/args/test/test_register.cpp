b63172c569f0afa3ffa912db92f5391fbabc1cf9
Niket Naidu <coder137@gmail.com>
2021-09-25 00:03:34 -0700

    Added Register_Build test

--- buildcc/lib/args/test/test_register.cpp
+++ buildcc/lib/args/test/test_register.cpp
@@ -74,6 +74,54 @@ TEST(RegisterTestGroup, Register_Clean) {
   mock().checkExpectations();
 }
 
+TEST(RegisterTestGroup, Register_Build) {
+  std::vector<const char *> av{
+      "",
+      "--config",
+      "configs/basic_parse.toml",
+  };
+  int argc = av.size();
+
+  buildcc::Args args;
+  buildcc::Args::ToolchainArg gcc_toolchain;
+  buildcc::Args::ToolchainArg msvc_toolchain;
+  args.AddToolchain("gcc", "Generic gcc toolchain", gcc_toolchain);
+  args.AddToolchain("msvc", "Generic msvc toolchain", msvc_toolchain);
+  args.Parse(argc, av.data());
+
+  STRCMP_EQUAL(args.GetProjectRootDir().string().c_str(), "root");
+  STRCMP_EQUAL(args.GetProjectBuildDir().string().c_str(), "build");
+  CHECK(args.GetLogLevel() == buildcc::env::LogLevel::Trace);
+  CHECK_TRUE(args.Clean());
+
+  // Make dummy toolchain and target
+  buildcc::env::init(fs::current_path(), fs::current_path());
+  buildcc::base::Toolchain toolchain(buildcc::base::Toolchain::Id::Gcc, "", "",
+                                     "", "", "", "");
+  buildcc::base::Target target("dummyT", buildcc::base::TargetType::Executable,
+                               toolchain, "");
+
+  {
+    buildcc::Args::ToolchainState state{false, false};
+
+    buildcc::Register reg(args);
+    reg.Build(state, target,
+              [](buildcc::base::Target &target) { (void)target; });
+  }
+
+  {
+    buildcc::Args::ToolchainState state{true, true};
+
+    buildcc::Register reg(args);
+    mock().expectNCalls(1, "BuildTask_dummyT");
+    reg.Build(state, target,
+              [](buildcc::base::Target &target) { (void)target; });
+  }
+
+  buildcc::env::deinit();
+  mock().checkExpectations();
+}
+
 int main(int ac, char **av) {
   return CommandLineTestRunner::RunAllTests(ac, av);
 }
