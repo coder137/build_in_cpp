300c8a909f17521e2ced48efa29337b72469cc0f
Niket Naidu <coder137@gmail.com>
2021-09-27 21:48:17 -0700

    Added buildcc_find module

---
+++ buildcc/plugins/include/plugins/buildcc_find.h
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2021 Niket Naidu. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef PLUGINS_BUILDCC_FIND_H_
+#define PLUGINS_BUILDCC_FIND_H_
+
+#include <filesystem>
+#include <initializer_list>
+#include <string>
+
+#include "env/host_os.h"
+
+#include "fmt/format.h"
+
+namespace fs = std::filesystem;
+
+namespace buildcc::plugin {
+
+/**
+ * @brief By default searched the `PATH` environment variable for required
+ * package
+ *
+ */
+// TODO, Add support for regex matching
+class BuildccFind {
+public:
+  // TODO, Add more
+  enum class Type {
+    HostExecutable,
+    BuildccLibrary,
+    BuildccPlugin,
+  };
+
+public:
+  BuildccFind(const std::string &regex, Type type,
+              std::initializer_list<std::string> host_env_vars = {})
+      : regex_(regex), type_(type), host_env_vars_(host_env_vars) {
+    if constexpr (env::is_win()) {
+      regex_ = fmt::format("{}.exe", regex);
+    }
+  }
+  BuildccFind(const BuildccFind &) = delete;
+
+  void AddHostEnvVar(const std::string &host_env_var) {
+    host_env_vars_.push_back(host_env_var);
+  }
+  void AddHostEnvVars(std::initializer_list<std::string> host_env_vars) {
+    for (const auto &ev : host_env_vars) {
+      AddHostEnvVar(ev);
+    }
+  }
+
+  /**
+   * @brief Starts the search for package of type and name
+   *
+   */
+  bool Search();
+
+  //   Getters
+  const std::vector<fs::path> &GetPathMatches() { return target_matches_; }
+
+private:
+  bool SearchHostExecutable();
+
+private:
+  std::string regex_;
+  Type type_;
+
+  std::vector<std::string> host_env_vars_;
+
+  std::vector<fs::path> target_matches_;
+};
+
+} // namespace buildcc::plugin
+
+#endif
---
+++ buildcc/plugins/src/buildcc_find.cpp
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2021 Niket Naidu. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "plugins/buildcc_find.h"
+
+#include <cstdlib>
+#include <cstring>
+#include <regex>
+
+#include "env/logging.h"
+
+#include "target/path.h"
+
+namespace {
+constexpr const char *const kEnvVarNotFound =
+    "{} environment variable not found";
+constexpr const char *const kOsNotSupported =
+    "Search functionality does not support this operating system. Raise an "
+    "issue at https://github.com/coder137/build_in_cpp outlining your OS and "
+    "usecase";
+
+// Windows
+constexpr const char *const kWinEnvDelim = ";";
+
+// Linux
+constexpr const char *const kLinuxEnvDelim = ":";
+
+// TODO, Add Environment Variable delimiters for more operating systems
+// ; for windows
+// : for linux
+constexpr const char *const GetEnvVarDelim() {
+  if constexpr (buildcc::env::is_win()) {
+    return kWinEnvDelim;
+  } else if constexpr (buildcc::env::is_linux()) {
+    return kLinuxEnvDelim;
+  }
+
+  return nullptr;
+}
+
+std::vector<fs::path> SplitEnv(const char *env_ptr, const char *delim) {
+  std::vector<fs::path> env_paths;
+
+  std::string temp{env_ptr};
+  char *path = std::strtok(temp.data(), delim);
+  while (path != nullptr) {
+    env_paths.push_back(
+        buildcc::internal::Path::CreateNewPath(path).GetPathname());
+    path = std::strtok(nullptr, delim);
+  }
+
+  return env_paths;
+}
+
+std::vector<fs::path> SearchEnv(const std::string &host_env_var,
+                                const std::string &regex) {
+  char *path_ptr = std::getenv(host_env_var.c_str());
+  if (path_ptr == nullptr) {
+    buildcc::env::log_critical(__FUNCTION__,
+                               fmt::format(kEnvVarNotFound, host_env_var));
+    return {};
+  }
+
+  std::vector<fs::path> env_paths;
+  constexpr const char *const kDelim = GetEnvVarDelim();
+  if constexpr (kDelim == nullptr) {
+    buildcc::env::log_critical(__FUNCTION__, kOsNotSupported);
+    return {};
+  }
+  env_paths = SplitEnv(path_ptr, kDelim);
+
+  // DONE, Construct a directory iterator
+  // Only take the files
+  std::vector<fs::path> matches;
+  for (const auto &env_p : env_paths) {
+    std::error_code errcode;
+    const auto dir_iter = fs::directory_iterator(env_p, errcode);
+    if (errcode) {
+      buildcc::env::log_critical(env_p.string(), errcode.message());
+      continue;
+    }
+
+    for (const auto &dir_entry : dir_iter) {
+      if (!dir_entry.is_regular_file()) {
+        continue;
+      }
+
+      // Compare name_ with filename
+      std::string filename = dir_entry.path().filename().string();
+      bool match = std::regex_match(filename, std::regex(regex));
+      if (match) {
+        matches.push_back(dir_entry.path());
+      }
+    }
+  }
+
+  return matches;
+}
+
+} // namespace
+
+namespace buildcc::plugin {
+
+// Public
+
+bool BuildccFind::Search() {
+  switch (type_) {
+  case Type::HostExecutable:
+    return SearchHostExecutable();
+    break;
+  case Type::BuildccLibrary:
+  case Type::BuildccPlugin:
+  default:
+    // TODO, Add this functionality
+    break;
+  }
+
+  return false;
+}
+
+// Private
+
+bool BuildccFind::SearchHostExecutable() {
+  for (const auto &ev : host_env_vars_) {
+    std::vector<fs::path> matches = SearchEnv(ev, regex_);
+    target_matches_.insert(target_matches_.end(), matches.begin(),
+                           matches.end());
+  }
+
+  // TODO,
+  // for (const auto &asp : absolute_search_paths_) {
+  // }
+
+  return !target_matches_.empty();
+}
+
+} // namespace buildcc::plugin
