5a805b49b0c8c53fd63ac02fd181b66448c3af59
Niket Naidu <coder137@gmail.com>
2021-10-09 19:34:28 -0700

    Updated target_gcc with GccConfig and ConfigInterface

--- buildcc/targets/include/targets/target_gcc.h
+++ buildcc/targets/include/targets/target_gcc.h
@@ -19,26 +19,62 @@
 
 #include "target/target.h"
 
+#include "targets/target_config_interface.h"
+
 // TODO, Combine all of these into Target_gcc
 namespace buildcc {
 
 // Extensions
-constexpr std::string_view kGccStaticLibExt = ".a";
-constexpr std::string_view kGccDynamicLibExt = ".so";
+constexpr const char *const kGccExecutableExt = "";
+constexpr const char *const kGccStaticLibExt = ".a";
+constexpr const char *const kGccDynamicLibExt = ".so";
+
+constexpr const char *const kGccObjExt = ".o";
 
 // GCC
-constexpr std::string_view kGccPrefixIncludeDir = "-I";
-constexpr std::string_view kGccPrefixLibDir = "-L";
-constexpr std::string_view kGccGenericCompileCommand =
+constexpr const char *const kGccPrefixIncludeDir = "-I";
+constexpr const char *const kGccPrefixLibDir = "-L";
+constexpr const char *const kGccGenericCompileCommand =
     "{compiler} {preprocessor_flags} {include_dirs} {common_compile_flags} "
     "{compile_flags} -o {output} -c {input}";
-constexpr std::string_view kGccExecutableLinkCommand =
+constexpr const char *const kGccExecutableLinkCommand =
     "{cpp_compiler} {link_flags} {compiled_sources} -o {output} "
     "{lib_dirs} {lib_deps}";
-constexpr std::string_view kGccStaticLibLinkCommand =
+constexpr const char *const kGccStaticLibLinkCommand =
     "{archiver} rcs {output} {compiled_sources}";
-constexpr std::string_view kGccDynamicLibLinkCommand =
+constexpr const char *const kGccDynamicLibLinkCommand =
     "{cpp_compiler} -shared {link_flags} {compiled_sources} -o {output}";
+
+class GccConfig {
+public:
+  static base::Target::Config Executable() {
+    return DefaultGccConfig(kGccExecutableExt, kGccGenericCompileCommand,
+                            kGccExecutableLinkCommand);
+  }
+  static base::Target::Config StaticLib() {
+    return DefaultGccConfig(kGccDynamicLibExt, kGccGenericCompileCommand,
+                            kGccDynamicLibLinkCommand);
+  }
+  static base::Target ::Config DynamicLib() {
+    return DefaultGccConfig(kGccDynamicLibExt, kGccGenericCompileCommand,
+                            kGccDynamicLibLinkCommand);
+  }
+
+private:
+  static base::Target::Config
+  DefaultGccConfig(const std::string &target_ext,
+                   const std::string &compile_command,
+                   const std::string &link_command) {
+    base::Target::Config config;
+    config.target_ext = target_ext;
+    config.obj_ext = kGccObjExt;
+    std::string prefix_include_dir = kGccPrefixIncludeDir;
+    std::string prefix_lib_dir = kGccPrefixLibDir;
+    config.compile_command = compile_command;
+    config.link_command = link_command;
+    return config;
+  }
+};
 
 class ExecutableTarget_gcc : public base::Target {
 public:
@@ -46,7 +82,8 @@ public:
       const std::string &name, const base::Toolchain &toolchain,
       const std::filesystem::path &target_path_relative_to_root)
       : Target(name, base::TargetType::Executable, toolchain,
-               target_path_relative_to_root) {}
+               target_path_relative_to_root,
+               ConfigInterface<GccConfig>::Executable()) {}
 };
 
 class StaticTarget_gcc : public base::Target {
@@ -54,10 +91,8 @@ public:
   StaticTarget_gcc(const std::string &name, const base::Toolchain &toolchain,
                    const std::filesystem::path &target_path_relative_to_root)
       : Target(name, base::TargetType::StaticLibrary, toolchain,
-               target_path_relative_to_root) {
-    target_ext_ = kGccStaticLibExt;
-    link_command_ = kGccStaticLibLinkCommand;
-  }
+               target_path_relative_to_root,
+               ConfigInterface<GccConfig>::StaticLib()) {}
 };
 
 class DynamicTarget_gcc : public base::Target {
@@ -65,9 +100,8 @@ public:
   DynamicTarget_gcc(const std::string &name, const base::Toolchain &toolchain,
                     const std::filesystem::path &target_path_relative_to_root)
       : Target(name, base::TargetType::DynamicLibrary, toolchain,
-               target_path_relative_to_root) {
-    target_ext_ = kGccDynamicLibExt;
-    link_command_ = kGccDynamicLibLinkCommand;
+               target_path_relative_to_root,
+               ConfigInterface<GccConfig>::DynamicLib()) {
     AddCommonCompileFlag("-fpic");
   }
 };
