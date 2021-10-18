14b09dfc170f19c9c3c7e73ad1da0917730e37f9
Niket Naidu <coder137@gmail.com>
2021-10-17 22:16:55 -0700

    Update lib.cpp

--- buildcc/lib/target/src/target/lib.cpp
+++ buildcc/lib/target/src/target/lib.cpp
@@ -33,21 +33,21 @@ void Target::AddLibDirAbsolute(const fs::path &absolute_lib_dir) {
   env::log_trace(name_, __FUNCTION__);
 
   LockedAfterBuild();
-  current_lib_dirs_.insert(absolute_lib_dir);
+  storer_.current_lib_dirs.insert(absolute_lib_dir);
 }
 
 void Target::AddLibDep(const Target &lib_dep) {
   env::log_trace(name_, __FUNCTION__);
 
   LockedAfterBuild();
-  current_lib_deps_.user.insert(lib_dep.GetTargetPath());
+  storer_.current_lib_deps.user.insert(lib_dep.GetTargetPath());
 }
 
 void Target::AddLibDep(const std::string &lib_dep) {
   env::log_trace(name_, __FUNCTION__);
 
   LockedAfterBuild();
-  current_external_lib_deps_.insert(lib_dep);
+  storer_.current_external_lib_deps.insert(lib_dep);
 }
 
 } // namespace buildcc::base
