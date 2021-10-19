4b4d313f4d8be68f3618565a86867420e5f123fa
Niket Naidu <coder137@gmail.com>
2021-10-19 02:02:53 -0700

    Added hybrid pch example

---
+++ example/hybrid/pch/files/src/main.cpp
@@ -0,0 +1,22 @@
+#include "pch_cpp.h"
+
+int main() {
+  std::cout << "Hello World from test" << std::endl;
+
+  // -Wall -Werror flags throw error when unused variable
+  // int i = 0;
+
+#if RANDOM == 1
+  random_print();
+#endif
+
+  float f1 = 10.2;
+  float f2 = 1.0;
+  std::cout << "input a floating point number" << std::endl;
+  std::cin >> f2;
+
+  float value = pow(f1, f2);
+  std::cout << value << std::endl;
+
+  return 0;
+}
