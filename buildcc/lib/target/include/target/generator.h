57305fa294c76c92ef00579d25b5aea7ab8cabb9
Niket Naidu <coder137@gmail.com>
2021-11-05 21:04:25 -0700

    Updated generator with generator_root_dir and generator_build_dir
    - Added function for AddDefaultArgument, AddInput, AddOutput and AddCommand

--- buildcc/lib/target/include/target/generator.h
+++ buildcc/lib/target/include/target/generator.h
@@ -19,11 +19,14 @@
 
 #include <functional>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 #include "taskflow/taskflow.hpp"
 
 #include "env/env.h"
+
+#include "command/command.h"
 
 #include "target/builder_interface.h"
 
@@ -32,24 +35,33 @@
 
 namespace buildcc::base {
 
-// TODO, Add root and build path
 class Generator : public BuilderInterface {
 public:
   Generator(const std::string &name,
-            const fs::path &target_path_relative_to_root,
-            const internal::fs_unordered_set &inputs,
-            const internal::fs_unordered_set &outputs,
-            const std::vector<std::string> &commands, bool parallel = false)
-      : name_(name), loader_(name, target_path_relative_to_root),
-        current_input_files_({}, inputs), current_output_files_(outputs),
-        current_commands_(commands), parallel_(parallel) {
-    unique_id_ = name;
+            const fs::path &target_path_relative_to_root, bool parallel = false)
+      : name_(name), generator_root_dir_(env::get_project_root_dir() /
+                                         target_path_relative_to_root),
+        generator_build_dir_(env::get_project_build_dir() / name),
+        loader_(name, generator_build_dir_), parallel_(parallel) {
+    Initialize();
   }
   Generator(const Generator &generator) = delete;
 
-  // TODO, Add Global arguments
+  /**
+   * @brief Add default arguments for input, output and command requirements
+   *
+   * @param arguments Key-Value pair for arguments
+   * NOTE, Key must be a variable (lvalue) not a constant (rvalue)
+   *
+   */
+  void AddDefaultArguments(
+      const std::unordered_map<const char *, std::string> &arguments);
 
-  // TODO, Add your own custom command
+  void AddInput(const std::string &absolute_input_pattern);
+  void AddOutput(const std::string &absolute_output_pattern);
+  void AddCommand(
+      const std::string &command_pattern,
+      const std::unordered_map<const char *, std::string> &arguments = {});
 
   void Build() override;
 
@@ -58,6 +70,8 @@ public:
   tf::Taskflow &GetTaskflow() { return tf_; }
 
 private:
+  void Initialize();
+
   void GenerateTask();
   void Convert();
   void BuildGenerate();
@@ -75,6 +89,8 @@ private:
 private:
   // Constructor
   std::string name_;
+  fs::path generator_root_dir_;
+  fs::path generator_build_dir_;
   internal::GeneratorLoader loader_;
 
   // Serialization
@@ -83,6 +99,8 @@ private:
   std::vector<std::string> current_commands_;
   bool parallel_{false};
 
+  // Internal
+  Command command_;
   tf::Taskflow tf_;
 };
 
--- buildcc/lib/target/src/generator/generator.cpp
+++ buildcc/lib/target/src/generator/generator.cpp
@@ -22,12 +22,57 @@
 
 namespace buildcc::base {
 
+void Generator::AddDefaultArguments(
+    const std::unordered_map<const char *, std::string> &arguments) {
+  command_.AddDefaultArguments(arguments);
+}
+
+void Generator::AddInput(const std::string &absolute_input_pattern) {
+  std::string absolute_input_string =
+      command_.Construct(absolute_input_pattern);
+
+  current_input_files_.user.emplace(fs::path(absolute_input_string));
+}
+
+void Generator::AddOutput(const std::string &absolute_output_pattern) {
+  std::string absolute_output_string =
+      command_.Construct(absolute_output_pattern);
+  current_output_files_.emplace(fs::path(absolute_output_string));
+}
+
+void Generator::AddCommand(
+    const std::string &command_pattern,
+    const std::unordered_map<const char *, std::string> &arguments) {
+  std::string constructed_command =
+      command_.Construct(command_pattern, arguments);
+  current_commands_.emplace_back(std::move(constructed_command));
+}
+
 void Generator::Build() {
   (void)loader_.Load();
+
   GenerateTask();
 }
 
 // PRIVATE
+
+void Generator::Initialize() {
+  // Checks
+  env::assert_fatal(
+      env::is_init(),
+      "Environment is not initialized. Use the buildcc::env::init API");
+
+  //
+  fs::create_directories(generator_build_dir_);
+  command_.AddDefaultArguments({
+      {"gen_root_dir", generator_root_dir_.string()},
+      {"gen_build_dir", generator_build_dir_.string()},
+  });
+
+  //
+  unique_id_ = name_;
+  tf_.name(name_);
+}
 
 void Generator::Convert() { current_input_files_.Convert(); }
 
