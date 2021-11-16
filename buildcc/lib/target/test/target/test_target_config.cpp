2a9a8876d301965aedf2fd174ba54e02a5ccfc48
Niket Naidu <coder137@gmail.com>
2021-11-15 22:12:14 -0800

    Added test_target_config

---
+++ buildcc/lib/target/test/target/test_target_config.cpp
@@ -0,0 +1,38 @@
+#include "target/common/target_config.h"
+
+// NOTE, Make sure all these includes are AFTER the system and header includes
+#include "CppUTest/CommandLineTestRunner.h"
+#include "CppUTest/MemoryLeakDetectorNewMacros.h"
+#include "CppUTest/TestHarness.h"
+#include "CppUTest/Utest.h"
+
+// clang-format off
+TEST_GROUP(TargetConfigTestGroup)
+{
+};
+// clang-format on
+
+TEST(TargetConfigTestGroup, GetFileExt) {
+  buildcc::base::TargetConfig target_config;
+
+  buildcc::base::TargetFileExt ext;
+
+  ext = target_config.GetFileExt("file.asm");
+  CHECK(ext == buildcc::base::TargetFileExt::Asm);
+
+  ext = target_config.GetFileExt("file.c");
+  CHECK(ext == buildcc::base::TargetFileExt::C);
+
+  ext = target_config.GetFileExt("file.cpp");
+  CHECK(ext == buildcc::base::TargetFileExt::Cpp);
+
+  ext = target_config.GetFileExt("file.h");
+  CHECK(ext == buildcc::base::TargetFileExt::Header);
+
+  ext = target_config.GetFileExt("file.invalid");
+  CHECK(ext == buildcc::base::TargetFileExt::Invalid);
+}
+
+int main(int ac, char **av) {
+  return CommandLineTestRunner::RunAllTests(ac, av);
+}
