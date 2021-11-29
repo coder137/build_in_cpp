cb30ebf1d8e2485a865644b08187ba6fdb60d9e2
Niket Naidu <coder137@gmail.com>
2021-11-29 00:52:26 -0800

    Update test_generator.cpp

--- buildcc/lib/target/test/target/test_generator.cpp
+++ buildcc/lib/target/test/target/test_generator.cpp
@@ -343,6 +343,28 @@ TEST(GeneratorTestGroup, Generator_FailedGenerateConvert) {
   buildcc::env::set_task_state(buildcc::env::TaskState::SUCCESS);
 }
 
+TEST(GeneratorTestGroup, Generator_FailedGenerateCommand) {
+  constexpr const char *const NAME = "FailedGenerateCommand";
+  buildcc::base::Generator generator(NAME, "", false);
+
+  generator.AddDefaultArguments({
+      {"compiler", "gcc"},
+  });
+
+  generator.AddInput("{gen_root_dir}/dummy_main.c");
+  generator.AddOutput("{gen_build_dir}/dummy_main.exe");
+  generator.AddCommand("{compiler} -o {gen_build_dir}/dummy_main.exe "
+                       "{gen_root_dir}/dummy_main.c");
+
+  buildcc::m::CommandExpect_Execute(1, false);
+  generator.Build();
+  buildcc::base::m::GeneratorRunner(generator);
+
+  mock().checkExpectations();
+
+  buildcc::env::set_task_state(buildcc::env::TaskState::SUCCESS);
+}
+
 int main(int ac, char **av) {
   fs::remove_all(BUILD_DIR);
   buildcc::env::init(fs::current_path() / "data", BUILD_DIR);
