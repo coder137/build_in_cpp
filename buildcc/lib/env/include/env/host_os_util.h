f7d3a51ce1b392bda9c4e6f3287edac6c962453b
Niket Naidu <coder137@gmail.com>
2021-12-04 19:59:41 -0800

    Create host_os_util.h

---
+++ buildcc/lib/env/include/env/host_os_util.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2021 Niket Naidu. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ENV_HOST_OS_UTIL_H_
+#define ENV_HOST_OS_UTIL_H_
+
+#include "host_os.h"
+#include "logging.h"
+
+namespace buildcc::env {
+
+// OS Path delimiters
+constexpr const char *const kWinEnvDelim = ";";
+constexpr const char *const kUnixEnvDelim = ":";
+
+/**
+ * @brief Get the OS EnvVariable delimiter
+ * ; for windows
+ * : for linux, unix and mac
+ *
+ * @return constexpr char const* for supported operating systems
+ * @return nullptr otherwise with a critical message to raise an issue
+ */
+inline constexpr char const *get_os_envvar_delim() {
+  if constexpr (is_win()) {
+    return kWinEnvDelim;
+  } else if constexpr (is_linux() || is_unix() || is_mac()) {
+    return kUnixEnvDelim;
+  }
+  env::log_critical(__FUNCTION__,
+                    "Unknown operating system, returning nullptr. Raise an "
+                    "issue at http://github.com/coder137/build_in_cpp");
+  return nullptr;
+}
+
+} // namespace buildcc::env
+
+#endif
