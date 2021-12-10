6fca2332b2d225773a053bb8e51c6700f14624f0
Niket Naidu <coder137@gmail.com>
2021-12-10 02:55:36 -0800

    Added BuildBuildCC to build_buildcc.h

--- bootstrap/include/bootstrap/build_buildcc.h
+++ bootstrap/include/bootstrap/build_buildcc.h
@@ -19,6 +19,13 @@
 
 #include "buildcc.h"
 
+#include "build_cli11.h"
+#include "build_flatbuffers.h"
+#include "build_fmtlib.h"
+#include "build_spdlog.h"
+#include "build_taskflow.h"
+#include "build_tpl.h"
+
 namespace buildcc {
 
 void schema_gen_cb(BaseGenerator &generator, const BaseTarget &flatc_exe);
@@ -27,6 +34,32 @@ void buildcc_cb(BaseTarget &target, const BaseGenerator &schema_gen,
                 const TargetInfo &flatbuffers_ho, const TargetInfo &fmt_ho,
                 const TargetInfo &spdlog_ho, const TargetInfo &cli11_ho,
                 const TargetInfo &taskflow_ho, const BaseTarget &tpl);
+
+/**
+ * @brief
+ *
+ */
+class BuildBuildCC {
+public:
+  BuildBuildCC(Register &reg, const BaseToolchain &toolchain,
+               const TargetEnv &env)
+      : reg_(reg), toolchain_(toolchain), env_(env) {}
+  BuildBuildCC(const BuildBuildCC &) = delete;
+
+  void Setup(const ArgToolchainState &state);
+
+  // Getters
+  StaticTarget_generic &GetBuildcc() {
+    return storage_.Ref<StaticTarget_generic>("libbuildcc");
+  }
+
+private:
+  Register &reg_;
+  const BaseToolchain &toolchain_;
+  TargetEnv env_;
+
+  PersistentStorage storage_;
+};
 
 } // namespace buildcc
 
