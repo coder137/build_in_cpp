0f8b514bdedb03d5ee617d151afa6cfd42dcb2c5
Niket Naidu <coder137@gmail.com>
2021-12-19 06:32:22 -0800

    Updated sphinx and conf with default_project options

--- cmake/tool/doxygen.cmake
+++ cmake/tool/doxygen.cmake
@@ -22,7 +22,7 @@ if (${BUILDCC_DOCUMENTATION})
         REQUIRED
     )
     add_custom_target(sphinx_documentation
-    COMMAND ${sphinx_build} -b html -Dbreathe_projects.cpp_sphinx_documentation=${CMAKE_CURRENT_BINARY_DIR}/xml . ../output
+    COMMAND ${sphinx_build} -b html -Dbreathe_projects.buildcc_documentation=${CMAKE_CURRENT_BINARY_DIR}/xml . ../output
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/docs/source
     VERBATIM USES_TERMINAL
     )
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,11 @@ release = '0.1.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "breathe"
 ]
+
+breathe_default_project = "buildcc_documentation"
+breathe_default_members = ("members", "undoc-members")
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
