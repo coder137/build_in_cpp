8bdc061afbc7d4d38e29f7760a063d8c18b564d2
Niket Naidu <coder137@gmail.com>
2022-01-01 20:36:17 -0800

    Renamed base::Target to BaseTarget

--- example/hybrid/foolib/build.foo.cpp
+++ example/hybrid/foolib/build.foo.cpp
@@ -1,6 +1,6 @@
 #include "build.foo.h"
 
-void fooTarget(buildcc::base::Target &target, const fs::path &relative_path) {
+void fooTarget(buildcc::BaseTarget &target, const fs::path &relative_path) {
   target.AddSource(relative_path / "src/foo.cpp");
   target.AddIncludeDir(relative_path / "src", true);
 }
