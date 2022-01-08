d978a560105c2bd3c83bcc70fd51be55216806e9
Niket Naidu <coder137@gmail.com>
2022-01-08 05:08:40 -0800

    Update args_setup.cpp

--- buildexe/src/args_setup.cpp
+++ buildexe/src/args_setup.cpp
@@ -38,42 +38,54 @@ void setup_arg_buildexe_mode(Args &args, BuildExeMode &out) {
 
 // TODO, Add subcommand [build.info]
 void setup_arg_target_info(Args &args, ArgTargetInfo &out) {
+  constexpr const char *const kProjectInfo = "Project Info";
   auto &app = args.Ref();
 
-  app.add_option("--name", out.name, "Provide Target name")->required();
+  auto *project_info_app = app.add_option_group(kProjectInfo);
 
-  app.add_option("--type", out.type, "Provide Target Type")
+  project_info_app->add_option("--name", out.name, "Provide Target name")
+      ->required();
+
+  project_info_app->add_option("--type", out.type, "Provide Target Type")
       ->transform(CLI::CheckedTransformer(kTargetTypeMap, CLI::ignore_case))
       ->required();
 
-  app.add_option("--relative_to_root", out.relative_to_root,
-                 "Provide Target relative to root")
+  project_info_app
+      ->add_option("--relative_to_root", out.relative_to_root,
+                   "Provide Target relative to root")
       ->required();
 }
 
 // TODO, Add subcommand [build.inputs]
 // TODO, Add group, group by sources, headers, inncludes on CLI
 void setup_arg_target_inputs(Args &args, ArgTargetInputs &out) {
+  constexpr const char *const kTargetInputs = "Target Inputs";
   auto &app = args.Ref();
 
-  app.add_option("--srcs", out.source_files, "Provide source files");
-  app.add_option("--includes", out.include_dirs, "Provide include dirs");
+  auto *target_inputs_app = app.add_option_group(kTargetInputs);
 
-  app.add_option("--lib_dirs", out.lib_dirs, "Provide lib dirs");
-  app.add_option("--external_libs", out.external_lib_deps,
-                 "Provide external libs");
+  target_inputs_app->add_option("--srcs", out.source_files,
+                                "Provide source files");
+  target_inputs_app->add_option("--includes", out.include_dirs,
+                                "Provide include dirs");
 
-  app.add_option("--preprocessor_flags", out.preprocessor_flags,
-                 "Provide Preprocessor flags");
-  app.add_option("--common_compile_flags", out.common_compile_flags,
-                 "Provide CommonCompile Flags");
-  app.add_option("--asm_compile_flags", out.asm_compile_flags,
-                 "Provide AsmCompile Flags");
-  app.add_option("--c_compile_flags", out.c_compile_flags,
-                 "Provide CCompile Flags");
-  app.add_option("--cpp_compile_flags", out.cpp_compile_flags,
-                 "Provide CppCompile Flags");
-  app.add_option("--link_flags", out.link_flags, "Provide Link Flags");
+  target_inputs_app->add_option("--lib_dirs", out.lib_dirs, "Provide lib dirs");
+  target_inputs_app->add_option("--external_libs", out.external_lib_deps,
+                                "Provide external libs");
+
+  target_inputs_app->add_option("--preprocessor_flags", out.preprocessor_flags,
+                                "Provide Preprocessor flags");
+  target_inputs_app->add_option("--common_compile_flags",
+                                out.common_compile_flags,
+                                "Provide CommonCompile Flags");
+  target_inputs_app->add_option("--asm_compile_flags", out.asm_compile_flags,
+                                "Provide AsmCompile Flags");
+  target_inputs_app->add_option("--c_compile_flags", out.c_compile_flags,
+                                "Provide CCompile Flags");
+  target_inputs_app->add_option("--cpp_compile_flags", out.cpp_compile_flags,
+                                "Provide CppCompile Flags");
+  target_inputs_app->add_option("--link_flags", out.link_flags,
+                                "Provide Link Flags");
 }
 
 void setup_arg_script_mode(Args &args, ArgScriptInfo &out) {
