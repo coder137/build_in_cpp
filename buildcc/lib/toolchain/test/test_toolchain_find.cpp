413c8f81a7046ca27930a63b42b423f3f4042d6e
Niket Naidu <coder137@gmail.com>
2022-03-18 00:53:14 -0700

    Added test_toolchain_find

---
+++ buildcc/lib/toolchain/test/test_toolchain_find.cpp
@@ -0,0 +1,65 @@
+#include <cstdlib>
+
+#include "toolchain/toolchain.h"
+
+#include "env/host_os.h"
+
+#include "expect_command.h"
+
+#include "mock_command_copier.h"
+
+// NOTE, Make sure all these includes are AFTER the system and header includes
+#include "CppUTest/CommandLineTestRunner.h"
+#include "CppUTest/MemoryLeakDetectorNewMacros.h"
+#include "CppUTest/TestHarness.h"
+#include "CppUTest/Utest.h"
+#include "CppUTestExt/MockSupport.h"
+
+// clang-format off
+TEST_GROUP(ToolchainFindTestGroup)
+{
+  void teardown() {
+    mock().checkExpectations();
+    mock().clear();
+  }
+};
+// clang-format on
+
+// NOTE, We are mocking the environment instead of actually querying it
+TEST(ToolchainFindTestGroup, VerifyToolchain_Gcc) {
+  buildcc::Toolchain gcc(buildcc::ToolchainId::Gcc, "gcc", "as", "gcc", "g++",
+                         "ar", "ld");
+
+  std::string putenv_str = fmt::format("CUSTOM_BUILDCC_PATH={}/toolchains/gcc",
+                                       fs::current_path().string());
+  int put = putenv(putenv_str.data());
+  CHECK_TRUE(put == 0);
+  const char *custom_buildcc_path = getenv("CUSTOM_BUILDCC_PATH");
+  CHECK_TRUE(custom_buildcc_path != nullptr);
+  UT_PRINT(custom_buildcc_path);
+
+  buildcc::ToolchainFindConfig config;
+  config.env_vars.clear();
+  config.env_vars.insert("CUSTOM_BUILDCC_PATH");
+
+  buildcc::fs_unordered_set found_toolchains = gcc.Find(config);
+  CHECK_TRUE(!found_toolchains.empty());
+}
+
+int main(int ac, char **av) {
+  //   buildcc::env::m::VectorStringCopier copier;
+  //   mock().installCopier(TEST_VECTOR_STRING_TYPE, copier);
+
+  // NOTE, Check the GCC, MSVC and Clang compilers
+  // Create directory and populate it with gcc and cl executables
+  // Linux
+  // toolchains/gcc
+  // toolchains/clang
+
+  // Windows
+  // toolchains/msvc
+  // toolchains/mingw
+  // TODO, Check executables used in clang
+
+  return CommandLineTestRunner::RunAllTests(ac, av);
+}
