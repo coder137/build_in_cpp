b127cf698a207195a63d29d01a9b801e3c94cbbf
Niket Naidu <coder137@gmail.com>
2022-03-19 17:58:03 -0700

    Update toolchain_find.h

--- buildcc/lib/toolchain/include/toolchain/api/toolchain_find.h
+++ buildcc/lib/toolchain/include/toolchain/api/toolchain_find.h
@@ -28,11 +28,23 @@ namespace fs = std::filesystem;
 
 namespace buildcc {
 
+/**
+ * @brief Configure the behaviour of Toolchain::Find API. By default searches
+ * the directories mentioned in the ENV{PATH} variable to find the toolchain.
+ * @param absolute_search_paths absolute_search_paths expect directories that
+ * are iterated for exact toolchain matches
+ * @param env_vars env_vars contain paths that are seperated by OS delimiter.
+ * These are converted to paths and searched similarly to absolute_search_paths
+ * <br>
+ */
 struct ToolchainFindConfig {
-  ToolchainFindConfig() = default;
+  ToolchainFindConfig(
+      const std::unordered_set<std::string> &env_vars = {"PATH"},
+      const fs_unordered_set &absolute_search_paths = {})
+      : env_vars(env_vars), absolute_search_paths(absolute_search_paths) {}
 
+  std::unordered_set<std::string> env_vars;
   fs_unordered_set absolute_search_paths;
-  std::unordered_set<std::string> env_vars{"PATH"};
 };
 
 template <typename T> class ToolchainFind {
