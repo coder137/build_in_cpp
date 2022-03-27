1f47468aa6abaefc8730515f8b41ac26dbf4cb6b
Niket Naidu <coder137@gmail.com>
2022-03-26 20:39:10 -0700

    Updated env.h

--- buildcc/lib/env/src/env.cpp
+++ buildcc/lib/env/src/env.cpp
@@ -17,14 +17,6 @@
 #include "env/env.h"
 #include "env/logging.h"
 
-// namespace {
-
-// fs::path root_dir_{""};
-// fs::path build_dir_{""};
-// bool init_ = false;
-
-// } // namespace
-
 namespace buildcc {
 
 void Project::Init(const fs::path &project_root_dir,
@@ -67,32 +59,3 @@ fs::path &Project::GetStaticBuildDir() {
 }
 
 } // namespace buildcc
-
-// namespace buildcc::env {
-
-// void init(const fs::path &project_root_dir, const fs::path
-// &project_build_dir) {
-//   // State
-//   root_dir_ = project_root_dir;
-//   build_dir_ = project_build_dir;
-//   root_dir_.make_preferred();
-//   build_dir_.make_preferred();
-
-//   init_ = true;
-
-//   // Logging
-//   set_log_pattern("%^[%l]%$ %v");
-//   set_log_level(LogLevel::Info);
-// }
-
-// void deinit() {
-//   root_dir_ = "";
-//   build_dir_ = "";
-//   init_ = false;
-// }
-
-// bool is_init(void) { return init_; }
-// const fs::path &get_project_root_dir() { return root_dir_; }
-// const fs::path &get_project_build_dir() { return build_dir_; }
-
-// } // namespace buildcc::env
