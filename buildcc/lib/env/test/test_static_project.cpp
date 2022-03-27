b844587c00ee59253f57de81ff8d6a37a37a8c0d
Niket Naidu <coder137@gmail.com>
2022-03-26 20:54:53 -0700

    Update test_static_project.cpp

--- buildcc/lib/env/test/test_static_project.cpp
+++ buildcc/lib/env/test/test_static_project.cpp
@@ -23,5 +23,6 @@ TEST(StaticProjectTestGroup, ProjectInitialized) {
 }
 
 int main(int ac, char **av) {
+  MemoryLeakWarningPlugin::turnOffNewDeleteOverloads();
   return CommandLineTestRunner::RunAllTests(ac, av);
 }
