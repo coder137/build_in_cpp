e78e585f36b6708e192e9f3436772c656da45b6e
Niket Naidu <coder137@gmail.com>
2022-03-27 15:59:57 -0700

    Update args_setup.cpp

--- buildexe/src/args_setup.cpp
+++ buildexe/src/args_setup.cpp
@@ -33,6 +33,7 @@ static const std::unordered_map<const char *, TargetType> kTargetTypeMap{
 };
 
 void BuildExeArgs::Setup() {
+  Args::Init();
   Args::AddToolchain("host", "Host Toolchain", host_toolchain_arg_);
   SetupBuildMode();
   SetupTargetInfo();
