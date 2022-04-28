634a7a0420d3454425feeca046b9872687c18b85
Niket Naidu <coder137@gmail.com>
2022-04-27 20:00:32 -0700

    Added toolchain infos

---
+++ buildcc/toolchains/include/toolchains/toolchain_infos.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2021-2022 Niket Naidu. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef TOOLCHAINS_TOOLCHAIN_INFOS_H_
+#define TOOLCHAINS_TOOLCHAIN_INFOS_H_
+
+#include <unordered_map>
+
+#include "toolchain/toolchain.h"
+
+namespace buildcc {
+
+class GlobalToolchainInfo {
+public:
+  static const ToolchainInfoFunc &Get(ToolchainId id);
+
+private:
+  static std::unordered_map<ToolchainId, ToolchainInfoFunc>
+      global_toolchain_info_func_;
+};
+
+} // namespace buildcc
+
+#endif
