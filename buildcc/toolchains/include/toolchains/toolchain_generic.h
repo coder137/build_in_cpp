6f549c414c2610384d16289a00fe47ac952e8477
Niket Naidu <coder137@gmail.com>
2022-04-28 19:44:24 -0700

    Removed compile time toolchain_generic. Make sure that constructors are uniform

--- buildcc/toolchains/include/toolchains/toolchain_generic.h
+++ buildcc/toolchains/include/toolchains/toolchain_generic.h
@@ -30,37 +30,7 @@ namespace buildcc {
 class Toolchain_generic {
 public:
   /**
-   * @brief Compile time way to create a generic toolchain instance
-   *
-   * @return Toolchain& Returns the BaseToolchain with necessary virtual
-   * function overrides
-   * Asserts fatal if ToolchainId is not supported
-   */
-  template <ToolchainId id, typename... Params>
-  static Toolchain &New(const std::string &identifier, Params &&...params) {
-    Toolchain *toolchain{nullptr};
-
-    if constexpr (id == ToolchainId::Gcc) {
-      toolchain = AddIf<Toolchain_gcc>(identifier, identifier,
-                                       std::forward<Params>(params)...);
-    }
-
-    if constexpr (id == ToolchainId::Msvc) {
-      toolchain = AddIf<Toolchain_msvc>(identifier, identifier,
-                                        std::forward<Params>(params)...);
-    }
-
-    if constexpr (id == ToolchainId::MinGW) {
-      toolchain = AddIf<Toolchain_mingw>(identifier, identifier,
-                                         std::forward<Params>(params)...);
-    }
-
-    env::assert_fatal(toolchain, "Toolchain could not be created");
-    return *toolchain;
-  }
-
-  /**
-   * @brief Runtime time way to create a generic toolchain instance
+   * @brief Create a generic toolchain instance
    *
    * @return Toolchain& Returns the BaseToolchain with necessary virtual
    * function overrides
